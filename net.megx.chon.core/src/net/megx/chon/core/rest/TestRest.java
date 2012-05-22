82b370e1152349e73703c585d09c5942f7e6c14b
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-05-22 09:24:35 +0000

    refactoring

---
+++ net.megx.chon.core/src/net/megx/chon/core/rest/TestRest.java
@@ -0,0 +1,14 @@
+package net.megx.chon.core.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("test")
+public class TestRest {
+	@GET
+	@Produces("text/html")
+	public String test() {
+		return "Hello TestRest";
+	}
+}
