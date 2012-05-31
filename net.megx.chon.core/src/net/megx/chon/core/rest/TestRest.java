faa97e965cf28502732d44817b8bcb32dbbbb3ab
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-05-31 08:57:50 +0000

    adding logger to test service

--- net.megx.chon.core/src/net/megx/chon/core/rest/TestRest.java
+++ net.megx.chon.core/src/net/megx/chon/core/rest/TestRest.java
@@ -4,11 +4,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 @Path("test")
 public class TestRest {
+	private static final Log log = LogFactory.getLog(TestRest.class);
+	
 	@GET
 	@Produces("text/html")
 	public String test() {
+		log.debug("Entering test metod in " + TestRest.class);
 		return "Hello TestRest";
 	}
 }
