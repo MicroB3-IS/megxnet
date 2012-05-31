634666fdf4f1ce8dbed3f5482533cc3552fe116c
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-05-31 08:56:33 +0000

    changed activators to extend JCRAppConfgEnabledActivator since only config is used and not res/tpl folders.

--- megx.net/trunk/net.megx.security.filter/src/net/megx/security/filter/Activator.java
+++ megx.net/trunk/net.megx.security.filter/src/net/megx/security/filter/Activator.java
@@ -1,18 +1,27 @@
 package net.megx.security.filter;
 
-import org.chon.cms.core.JCRApplication;
-import org.chon.cms.core.ResTplConfiguredActivator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.core.JCRAppConfgEnabledActivator;
+import org.json.JSONObject;
+import org.osgi.framework.BundleContext;
 
-public class Activator extends ResTplConfiguredActivator {
+public class Activator extends JCRAppConfgEnabledActivator {
+	private static final Log log = LogFactory.getLog(Activator.class);
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		JSONObject cfg = getConfig();
+		log.debug(cfg.optString("exampleProperty", "default value"));
+	}
 
 	@Override
-	protected void registerExtensions(JCRApplication app) {
-		// TODO register extension
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
 	}
 
 	@Override
 	protected String getName() {
 		return "net.megx.security.filter";
 	}
-	
 }
