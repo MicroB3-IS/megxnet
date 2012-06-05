5441031b1ca439c775a5d0cf55fdfe4bfaa9c00d
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-05 06:50:11 +0000

    Merge branch 'mb3-home' of ssh://62.162.67.150:7799/home/pavle/projects/java/work-mpi/mb3-git-clone into mb3-work

---
+++ megx.net/trunk/net.megx.security.filter/src/net/megx/security/filter/http/TemplatePageRenderer.java
@@ -0,0 +1,31 @@
+package net.megx.security.filter.http;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.model.content.IContentNode;
+import org.chon.cms.model.content.INodeRenderer;
+import org.chon.web.api.Application;
+import org.chon.web.api.Request;
+import org.chon.web.api.Response;
+import org.chon.web.api.ServerInfo;
+
+public class TemplatePageRenderer implements INodeRenderer{
+	
+	private Log log = LogFactory.getLog(getClass());
+	@Override
+	public void render(IContentNode contentNode, Request request, Response response,
+			Application application, ServerInfo serverInfo) {
+		System.out.println("+++++++++++ here +++++++++++++++");
+		log.debug("Ok, got to render.");
+		if(contentNode instanceof TemplatePageNode){
+			log.debug("Trying to render...");
+			try {
+				((TemplatePageNode)contentNode).process(request, response, serverInfo);
+				log.debug("Rendered, no problem.");
+			} catch (Exception e) {
+				log.error(e);
+			}
+		}
+	}
+
+}
