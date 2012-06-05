241cd8ba6ad98de4d25e76dfb0e935a8450d9a9f
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-05 09:43:22 +0000

    removed undefined ctor for java 6

--- net.megx.security.filter/src/net/megx/security/filter/SecurityException.java
+++ net.megx.security.filter/src/net/megx/security/filter/SecurityException.java
@@ -34,11 +34,6 @@ public class SecurityException extends Exception{
 		super();
 	}
 
-	public SecurityException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 	public SecurityException(String message, Throwable cause) {
 		super(message, cause);
 	}
