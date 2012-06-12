ec2aaeb368cbcc5610915dfc241e47e4433b9a86
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-12 10:59:18 +0000

    many minor changes

--- megx.net/trunk/net.megx.pubmap/src/net/megx/pubmap/Activator.java
+++ megx.net/trunk/net.megx.pubmap/src/net/megx/pubmap/Activator.java
@@ -40,32 +40,5 @@ public class Activator extends ResTplConfiguredActivator {
 		return "net.megx.pubmap";
 	}
 	
-	private void trackMVCService() {
-		ServiceTracker t = new ServiceTracker(getBundleContext(),
-				LightMVCService.class.getName(), null) {
-			@Override
-			public Object addingService(ServiceReference reference) {
-				LightMVCService mvc = (LightMVCService) super.addingService(reference);
-				registerMVCModel(mvc);
-				return mvc;
-			}
 
-			@Override
-			public void removedService(ServiceReference reference,
-					Object service) {
-			}
-		};
-		t.open();
-	}
-
-	protected void registerMVCModel(LightMVCService mvc) {
-		Map<String, Class<? extends AbstractAction>> actions = new HashMap<String, Class<? extends AbstractAction>>();
-		PubMapActions.registerActions(actions);
-		try {
-			mvc.setupController(PubMapActions.ACTIONS_ROOT, actions);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
