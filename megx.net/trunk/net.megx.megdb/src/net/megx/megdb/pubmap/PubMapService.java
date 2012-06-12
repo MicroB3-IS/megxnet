d3d80c5d46fdbc1a5d8e255ab5219e2dd3814bc1
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-12 09:50:10 +0000

    adding PubMapArticle, getAllArticles rest service adding gson lib to megdb and export com.google.gson packages

--- megx.net/trunk/net.megx.megdb/src/net/megx/megdb/pubmap/PubMapService.java
+++ megx.net/trunk/net.megx.megdb/src/net/megx/megdb/pubmap/PubMapService.java
@@ -1,5 +1,10 @@
 package net.megx.megdb.pubmap;
 
-public interface PubMapService {
+import java.util.List;
 
+public interface PubMapService {
+	//TODO: what if huge list, 
+	// 1. return iterator instead of list 
+	// 2. add arguments start and limit
+	public List<PubMapArticle> getAllArticles();
 }
--- megx.net/trunk/net.megx.pubmap/src/net/megx/pubmap/Activator.java
+++ megx.net/trunk/net.megx.pubmap/src/net/megx/pubmap/Activator.java
@@ -3,12 +3,15 @@ package net.megx.pubmap;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.megx.pubmap.rest.PubMapRest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
 import org.chon.cms.light.mvc.AbstractAction;
 import org.chon.cms.light.mvc.LightMVCService;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 
@@ -20,11 +23,16 @@ public class Activator extends ResTplConfiguredActivator {
 	protected void registerExtensions(JCRApplication app) {
 		log.debug("Start PubMap bundle");
 		
-		trackMVCService();
+		registerRestServices(getBundleContext());
+		//trackMVCService();
 		
 		PubMapExtension pubMapExt = new PubMapExtension();
 		log.debug("Registering pubmap extension.");
 		app.regExtension("pubmap", pubMapExt);
+	}
+
+	private void registerRestServices(BundleContext bundleContext) {
+		bundleContext.registerService(PubMapRest.class.getName(), new PubMapRest(bundleContext), null);
 	}
 
 	@Override
