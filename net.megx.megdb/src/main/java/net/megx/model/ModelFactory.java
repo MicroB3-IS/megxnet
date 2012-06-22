87a444e91221b01c425ecbac73d719c26b1000e5
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-22 12:38:16 +0000

    adding create sample

--- net.megx.megdb/src/main/java/net/megx/model/ModelFactory.java
+++ net.megx.megdb/src/main/java/net/megx/model/ModelFactory.java
@@ -6,6 +6,7 @@ package net.megx.model;
 import net.megx.model.impl.PubMapArticle;
 import net.megx.model.impl.PubMapAuthor;
 import net.megx.model.impl.PubMapJournal;
+import net.megx.model.impl.PubMapSample;
 
 /**
  * @author rkottman
@@ -26,5 +27,9 @@ public class ModelFactory {
 
 	public static Journal createJournal() {
 		return new PubMapJournal();
+	}
+
+	public static Sample createSample() {
+		return new PubMapSample();
 	}
 }
