ac4d53f1e12bdc6a8eec1fa616e1468d9c8a99d5
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-06-25 07:17:08 +0000

    implemented date conversion from/to json for samples in article

--- megx.net/trunk/net.megx.pubmap/src/test/java/net/megx/pubmap/test/DateResolutionConvertorTest.java
+++ megx.net/trunk/net.megx.pubmap/src/test/java/net/megx/pubmap/test/DateResolutionConvertorTest.java
@@ -1,73 +1,68 @@
 package net.megx.pubmap.test;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
+import junit.framework.Assert;
+
 import net.megx.model.DateResolution;
+import net.megx.model.ModelFactory;
+import net.megx.model.Sample;
 import net.megx.pubmap.rest.json.SampleDTO;
+import net.megx.pubmap.rest.json.SampleDTO.DateRes;
 
 import org.junit.Test;
 
 public class DateResolutionConvertorTest {
 	
-	class DateRes {
-		DateResolution res;
-		Date date;
-	}
-	
-	public DateRes getDateRes(DateResolution [] ar_arr, String [] fields) {
-		return new DateRes();
-	}
-	
+
 	@Test
 	public void testFromDTOtoDAO() {
 		SampleDTO dto = new SampleDTO();
 		dto.setSamyear("2012");
-		dto.setSammonth("09");
-		dto.setSamday("1");
+		dto.setSammonth("01");
+		dto.setSamday("28");
+		dto.setSamhour("15");
 		
-		
-		DateResolution [] dr_arr = new DateResolution[] {
-				DateResolution.YEAR,
-				DateResolution.MONTH,
-				DateResolution.DAY,
-				DateResolution.HOUR,
-				DateResolution.MINUTE,
-				DateResolution.SECOND
-		};
-		
-		int [] cal_vals = new int[] {
-				Calendar.YEAR,
-				Calendar.MONTH,
-				Calendar.DATE,
-				Calendar.HOUR,
-				Calendar.MINUTE,
-				Calendar.SECOND
-		};
-		
-		String[] arr = new String [] {
-				dto.getSamyear(),
-				dto.getSammonth(),
-				dto.getSamday(),
-				dto.getSamhour(),
-				dto.getSammin(),
-				dto.getSamsec()
-		};
-		
-		DateResolution res = null;
-		Calendar cal = Calendar.getInstance();
-		cal.set(0, 0, 1, 0, 0, 0);
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i] == null || arr[i].trim().length()==0) {
-				break;
-			} else {
-				res = dr_arr[i];
-				cal.set(cal_vals[i], cal.get(cal_vals[i]) + Integer.valueOf(arr[i]));
-			}
+		try {
+			DateRes d = SampleDTO.calcDateRes(dto);
+			Calendar c = Calendar.getInstance();
+			c.setTime(d.getDate());
+			
+			// year
+			Assert.assertEquals(2012, c.get(Calendar.YEAR));
+			
+			//zero index for month in calendar
+			Assert.assertEquals(0, c.get(Calendar.MONTH));
+			
+			//day
+			Assert.assertEquals(28, c.get(Calendar.DAY_OF_MONTH));
+			
+			
+			// resolution
+			Assert.assertEquals(DateResolution.HOUR, d.getResolution());
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+	}
+	@Test 
+	public void testDAOtoDTO() throws ParseException {
+		Sample sample = ModelFactory.createSample();
+		Date date = SampleDTO.DATE_FORMAT.parse("2012/01/28 15:00:00");
+		sample.setDateTaken(date);
+		sample.setDateRes(DateResolution.HOUR);
+		SampleDTO dto = SampleDTO.fromDAO(sample);
+		Assert.assertEquals("2012", dto.getSamyear());
+		Assert.assertEquals("01", dto.getSammonth());
+		Assert.assertEquals("28", dto.getSamday());
+		Assert.assertEquals("15", dto.getSamhour());
+		Assert.assertEquals(null, dto.getSammin());
+		Assert.assertEquals(null, dto.getSamsec());
 		
-		System.out.println("Resolution: " + res + " Time: " + cal.getTime());
 	}
 	
 }
