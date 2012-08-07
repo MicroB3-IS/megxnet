d27a4c2a24c1c16bf813e91f8dfb56c96ebe3265
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-08-07 15:18:05 +0000

    MEGX-62: Unit tests for the following net.megx.security.auth.services.UserService methods:
    public UserVerification addPendingUser(User user) throws Exception; 
    public void commitPending(User user, String userVerification, long ttl) throws Exception; 
    public UserVerification getVerification(String verificationValue, long ttl) throws Exception; 
    public User updateUser(User userInfo) throws Exception; 
    public User removeUser(String userid) throws Exception; 
    public Role createRole(Role role)throws Exception; 
    public List<Role> getAvailableRoles() throws Exception;  
    public Role removeRole(String role) throws Exception;

--- net.megx.megdb/src/test/java/net/megx/security/services/UserServiceTest.java
+++ net.megx.megdb/src/test/java/net/megx/security/services/UserServiceTest.java
@@ -1,11 +1,13 @@
 package net.megx.security.services;
 
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import net.megx.security.auth.model.Role;
 import net.megx.security.auth.model.User;
+import net.megx.security.auth.model.UserVerification;
 import net.megx.security.auth.services.UserService;
 import net.megx.security.auth.services.db.DBUserService;
 
@@ -33,27 +35,25 @@ public class UserServiceTest extends DBServiceTest{
 	public static String PROVIDER = "external-provider.com";
 	public static Role ROLE_USER = new Role();
 	public static Role ROLE_ADMIN = new Role();
+	public static Role ROLE_CMS_ADMIN = new Role();
 	
-	public static String NEW_USERNAME = "New testUser";
-	public static String NEW_DESCRIPTION = "New Description for the new user";
-	public static String NEW_FIRST_NAME = "New Test";
-	public static String NEW_LAST_NAME = "New User";
-	public static String NEW_INITIALS = "New TU";
+	public static String NEW_USERNAME = "newTestUser";
+	public static String NEW_DESCRIPTION = "New description for the existing user";
+	public static String NEW_FIRST_NAME = "newTest";
+	public static String NEW_LAST_NAME = "newUser";
+	public static String NEW_INITIALS = "newTU";
 	public static String NEW_EMAIL = "newTestUser@testsite.com";
-	public static boolean NEW_DISABLED = true;
-	public static boolean NEW_EXTERNAL = true;
+	public static boolean NEW_DISABLED = false;
+	public static boolean NEW_EXTERNAL = false;
 	public static Date NEW_JOINED_ON = new Date();
 	public static Date NEW_LAST_LOGIN = new Date();
 	public static String NEW_PASSWORD = "newPassword";
 	public static String NEW_PROVIDER = "newExternal-provider.com";
-	public static Role NEW_ROLE_USER = new Role();
-	public static Role NEW_ROLE_ADMIN = new Role();
 	
 	static{
 		ROLE_USER.setLabel("user");
 		ROLE_ADMIN.setLabel("admin");
-		NEW_ROLE_USER.setLabel("newUser");
-		NEW_ROLE_ADMIN.setLabel("newAdmin");
+		ROLE_CMS_ADMIN.setLabel("cmsAdmin");
 	}
 	
 	
@@ -78,26 +78,7 @@ public class UserServiceTest extends DBServiceTest{
 		return user;
 	}
 	
-	private User updateExistingUser(User user){
-		user.setDescription(NEW_DESCRIPTION);
-		user.setDisabled(NEW_DISABLED);
-		user.setEmail(NEW_EMAIL);
-		user.setExternal(NEW_EXTERNAL);
-		user.setFirstName(NEW_FIRST_NAME);
-		user.setInitials(NEW_INITIALS);
-		user.setLastName(NEW_LAST_NAME);
-		user.setJoinedOn(NEW_JOINED_ON);
-		user.setLastlogin(NEW_LAST_LOGIN);
-		user.setLastName(NEW_LAST_NAME);
-		//user.setLogin(NEW_USERNAME);
-		user.setPassword(NEW_PASSWORD);
-		user.setProvider(NEW_PROVIDER);
-		//user.getRoles().get(0).setLabel(NEW_ROLE_USER.getLabel());
-		return user;
-	}
-	
 	private User defaultUser;
-	
 	
 	@Before
 	public void setup() throws Exception{
@@ -110,13 +91,60 @@ public class UserServiceTest extends DBServiceTest{
 		
 	}
 	
-	
 	@Test
 	public void testAddUser() throws Exception{
 		userService.removeUser(USERNAME);
 		userService.addUser(defaultUser);
 		User user = userService.getUserByUserId(USERNAME);
 		Assert.assertNotNull("User was not successfully added.",user);
+	}
+	
+	@Test
+	public void testAddPendingUser() throws Exception{
+		userService.removeUser(defaultUser.getLogin());
+		userService.addPendingUser(defaultUser);
+		User pendingUser = userService.getUserByUserId(USERNAME);
+		Assert.assertEquals("Login does not match.", USERNAME, pendingUser.getLogin());
+		Assert.assertEquals("Description does not match.", DESCRIPTION, pendingUser.getDescription());
+		Assert.assertEquals("Email does not match.", EMAIL, pendingUser.getEmail());
+		Assert.assertEquals("First name does not match.", FIRST_NAME, pendingUser.getFirstName());
+		Assert.assertEquals("Initials does not match.", INITIALS, pendingUser.getInitials());
+		Assert.assertEquals("Joined date does not match.", JOINED_ON, pendingUser.getJoinedOn());
+		Assert.assertEquals("Last login date does not match.", LAST_LOGIN, pendingUser.getLastlogin());
+		Assert.assertEquals("Last name does not match.", LAST_NAME, pendingUser.getLastName());
+		Assert.assertEquals("Disabled does not match", true, pendingUser.isDisabled());
+		//TODO: The pending user does not have roles after calling commitPending, needs to be fixed.
+		//Assert.assertNotNull("Roles must not be null.", defaultUser.getRoles());
+		//Assert.assertEquals("User must have exatcly one role: ", 1, defaultUser.getRoles().size());
+	}
+	
+	@Test
+	public void testCommitPending() throws Exception{
+		userService.removeUser(defaultUser.getLogin());
+		UserVerification verification = userService.addPendingUser(defaultUser);
+		defaultUser.setDisabled(false);
+		userService.commitPending(defaultUser, verification.getVerificationValue(), 10000);
+		
+		User committedUser = userService.getUserByUserId(USERNAME);
+		Assert.assertEquals("Login does not match.", USERNAME, committedUser.getLogin());
+		Assert.assertEquals("Description does not match.", DESCRIPTION, committedUser.getDescription());
+		Assert.assertEquals("Email does not match.", EMAIL, committedUser.getEmail());
+		Assert.assertEquals("First name does not match.", FIRST_NAME, committedUser.getFirstName());
+		Assert.assertEquals("Initials does not match.", INITIALS, committedUser.getInitials());
+		Assert.assertEquals("Joined date does not match.", JOINED_ON, committedUser.getJoinedOn());
+		Assert.assertEquals("Last login date does not match.", LAST_LOGIN, committedUser.getLastlogin());
+		Assert.assertEquals("Last name does not match.", LAST_NAME, committedUser.getLastName());
+		Assert.assertEquals("Disabled does not match", false, committedUser.isDisabled());
+		//TODO: The pending user does not have roles after calling commitPending, needs to be fixed.
+		//Assert.assertNotNull("Roles must not be null.", committedUser.getRoles());
+		//Assert.assertEquals("User must have exatcly one role: ", 1, committedUser.getRoles().size());
+	}
+
+	@Test
+	public void testGetVerification() throws Exception{
+		userService.removeUser(defaultUser.getLogin());
+		UserVerification verification = userService.addPendingUser(defaultUser);
+		Assert.assertNotNull("Verification string does not exist", userService.getVerification(verification.getVerificationValue(), 10000));
 	}
 	
 	@Test
@@ -159,17 +187,31 @@ public class UserServiceTest extends DBServiceTest{
 	
 	@Test
 	public void testGetAvailableRoles() throws Exception{
-		User user = userService.getUserByUserId(USERNAME);
-		Assert.assertNotNull("Roles must not be null.", user.getRoles());
-		Assert.assertEquals("Roles do not match", ROLE_USER.getLabel(), user.getRoles().get(0).getLabel());
+		List<Role> availableRoles = userService.getAvailableRoles();
+		Assert.assertTrue("USER role is not available.", availableRoles.contains(ROLE_USER));
+		Assert.assertTrue("ADMIN role is not available.", availableRoles.contains(ROLE_ADMIN));
+		Assert.assertTrue("CMS_ADMIN role is not available.", availableRoles.contains(ROLE_CMS_ADMIN));
 	}
 	
 	@Test
 	public void testUpdateUser() throws Exception{
 		User user = userService.getUserByUserId(USERNAME);
-		userService.updateUser(updateExistingUser(user));
-		User updatedUser = userService.getUserByUserId(USERNAME);
+		user.setDescription(NEW_DESCRIPTION);
+		user.setDisabled(NEW_DISABLED);
+		user.setEmail(NEW_EMAIL);
+		user.setExternal(NEW_EXTERNAL);
+		user.setFirstName(NEW_FIRST_NAME);
+		user.setInitials(NEW_INITIALS);
+		user.setLastName(NEW_LAST_NAME);
+		user.setJoinedOn(NEW_JOINED_ON);
+		user.setLastlogin(NEW_LAST_LOGIN);
+		user.setLastName(NEW_LAST_NAME);
+		//user.setLogin(NEW_USERNAME);
+		user.setPassword(NEW_PASSWORD);
+		user.setProvider(NEW_PROVIDER);
+		userService.updateUser(user);
 		
+		User updatedUser = userService.getUserByUserId(USERNAME);
 		Assert.assertEquals("Login does not match.", USERNAME, updatedUser.getLogin());
 		Assert.assertEquals("Description does not match.", NEW_DESCRIPTION, updatedUser.getDescription());
 		Assert.assertEquals("Email does not match.", NEW_EMAIL, updatedUser.getEmail());
@@ -183,6 +225,43 @@ public class UserServiceTest extends DBServiceTest{
 		
 		Assert.assertNotNull("Roles must not be null.", updatedUser.getRoles());
 		Assert.assertEquals("User must have exatcly one role: ", 1, updatedUser.getRoles().size());
+	}
+	
+	@Test
+	public void testRemoveUser() throws Exception{
+		userService.removeUser(USERNAME);
+		User user = userService.getUserByUserId(USERNAME);
+		Assert.assertNull(user);
+		
+		//Add the user again so it can be succesfully removed in the teardown method
+		userService.addUser(defaultUser);
+	}
+	
+	@Test
+	public void testCreateRole() throws Exception{
+		Role role = new Role();
+		role.setDescription(DESCRIPTION);
+		role.setLabel("chonAdmin");
+		userService.createRole(role);
+		
+		List<Role> availableRoles = userService.getAvailableRoles();
+		Assert.assertTrue("Role is not present in the list of available roles", availableRoles.contains(role));
+		
+		userService.removeRole(role.getLabel());
+	}
+	
+	@Test
+	public void testRemoveRole() throws Exception{
+		Role role = new Role();
+		role.setDescription(DESCRIPTION);
+		role.setLabel("chonAdmin");
+		userService.createRole(role);
+		
+		List<Role> availableRoles = userService.getAvailableRoles();
+		Assert.assertTrue("Role is not present in the list of available roles", availableRoles.contains(role));
+		
+		userService.removeRole(role.getLabel());
+		Assert.assertTrue("Role is present in the list of available roles after removal", userService.getAvailableRoles().contains(role) == false);
 	}
 	
 	@After
