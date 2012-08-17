1138d72be31b5950e2ce20523b7f4b6b1f3e069c
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-08-17 13:37:12 +0000

    adding dav credentials provider

--- megx.net/trunk/net.megx.was.webdav/src/net/megx/was/webdav/Activator.java
+++ megx.net/trunk/net.megx.was.webdav/src/net/megx/was/webdav/Activator.java
@@ -5,6 +5,8 @@ import java.util.Hashtable;
 import javax.jcr.Repository;
 import javax.servlet.Servlet;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
 import org.chon.cms.model.ContentModel;
@@ -12,6 +14,8 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator extends ResTplConfiguredActivator {
+	
+	private static final Log log = LogFactory.getLog(Activator.class);
 	
 	private static final String ALIAS = "/dav";
 	private ServiceRegistration servletContainerRegRef;
@@ -23,17 +27,14 @@ public class Activator extends ResTplConfiguredActivator {
 
 	@Override
 	protected void registerExtensions(JCRApplication app) {
-		System.out.println("*APP************** Activator.onAppAdded()");
-		
 		ContentModel cm = app.createContentModelInstance(getName());
 		Repository repository = cm.getSession().getRepository();
-		
-		System.out.println("!!!!!! Starting DAV activator!!!");
+		log.debug("Starting WebDav Servlet");
 		String serveltRoot = System.getProperty("dav.root", ALIAS);
 		Hashtable<String, String> props = new Hashtable<String, String>();
 		props.put("alias", serveltRoot);
 		props.put("init.resource-path-prefix", serveltRoot);
-		props.put("init.missing-auth-mapping", "");
+		props.put("init.missing-auth-mapping", "dav_user:dav_pass");
 		props.put("init.resource-config", "__config/config.xml");
 		SimpleWebdavServletImpl servlet = new SimpleWebdavServletImpl(repository);
 		servletContainerRegRef = getBundleContext().registerService(
