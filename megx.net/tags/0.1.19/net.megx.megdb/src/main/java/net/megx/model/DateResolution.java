42fa60e1829973ba6027515f8fe2c3be18f86e37
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-08-30 11:43:16 +0000

    Version 0.1.19 - stable base

---
+++ megx.net/tags/0.1.19/net.megx.megdb/src/main/java/net/megx/model/DateResolution.java
@@ -0,0 +1,17 @@
+package net.megx.model;
+
+public enum DateResolution {
+	MICROSECONDS, 
+	MILLISECONDS, 
+	SECOND, 
+	MINUTE, 
+	HOUR, 
+	DAY, 
+	WEEK, 
+	MONTH, 
+	QUARTER, 
+	YEAR, 
+	DECADE, 
+	CENTURY, 
+	MILLENNIUM
+}
