969feb6babaf48f982ab17af6a77be7ad3a03bea
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-11-15 13:38:49 +0000

    Six frame translation REST service

--- net.megx.ws.genome-services/src/net/megx/ws/genomes/SixFrameTranslation.java
+++ net.megx.ws.genome-services/src/net/megx/ws/genomes/SixFrameTranslation.java
@@ -1,7 +1,16 @@
 package net.megx.ws.genomes;
 
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
 
+import javax.jcr.RepositoryException;
+
+import net.megx.storage.StorageException;
+import net.megx.storage.StoredResource;
 import net.megx.ws.genomes.resources.WorkspaceAccess;
 /*
   ACTG
@@ -69,20 +78,153 @@ public class SixFrameTranslation extends BaseGenomeService{
 	 * @return
 	 * @author Mile Stefanovski
 	 */
-	public int translate(byte [] chunk, byte [] chunkOut){
-		int bl = chunk.length/3;
+	public int translate(byte [] chunk, int start, int size, byte [] chunkOut){
+		int bl = (size-start)/3;
 		bl*=3;
 		int j = 0;
-		for(int i = 0; i < bl; i+=3,j++){
+		for(int i = start; i < bl; i+=3,j++){
 			chunkOut[j] = (byte)AMINO_ACIDS[((chunk[i]&6)<<3)|((chunk[i+1]&6)<<1) | ((chunk[i+2]>>1)&3)];
 		}
-		if(bl < chunk.length){
+		if(bl < size){
 			chunkOut[j] = (byte)AMINO_ACIDS[AMINO_ACIDS.length-1];
 			j++;
 		}
 		return j;
 	}
 	
+	public int translateReverseFrame(byte [] chunk, int start, int size, byte [] chunkOut){
+		int bl = (size-start)/3;
+		bl*=3;
+		int j = 0;
+		for(int i = size-1-start; i >(size-bl); i-=3,j++){
+			chunkOut[j] = (byte)AMINO_ACIDS[((chunk[i]&6)<<3)|((chunk[i-1]&6)<<1) | ((chunk[i-2]>>1)&3)];
+		}
+		if(bl < size){
+			chunkOut[j] = (byte)AMINO_ACIDS[AMINO_ACIDS.length-1];
+			j++;
+		}
+		return j;
+	}
+	
+	
+	public int translateOnFrame(int frame, byte [] input, byte [] out) throws IOException{
+		if(frame > 0){
+			return translate(input, frame-1, input.length-frame-1, out);
+		}else{
+			frame = -frame-1;
+			return translateReverseFrame(input, frame, input.length-frame, out);
+		}
+	}
+	
+	
+	public void sixFrameTranslate(String user, int [] frames, String inputFile, 
+			OutputStream output, boolean includeOriginalFASTA, int lineSize) throws RepositoryException, 
+		URISyntaxException, StorageException, IllegalArgumentException, IOException{
+		for(int frame: frames){
+			if(frame < -3 || frame == 0 || frame > 3)
+				throw new IllegalArgumentException("Invalid frame: " + frame);
+		}
+		
+		StoredResource input = getAccess().getResource(user, inputFile);
+		String fastaHeader = null;
+		StringBuffer sequence = new StringBuffer();
+		String line = null;
+		
+		InputStream srStream = input.read();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(srStream));
+		while((line = reader.readLine())!= null){
+			line = line.trim();
+			if(line.startsWith(">")){
+				fastaHeader = line;
+				if(sequence.length() > 0){
+					// call translate function here
+					doTranslate(frames, fastaHeader, sequence.toString().getBytes(), 
+							output, includeOriginalFASTA, lineSize);
+					sequence = new StringBuffer();
+					fastaHeader = null;
+				}
+			}else if(!"".equals(line)){
+				sequence.append(line);
+			}
+		}
+		if(sequence.length() > 0 && fastaHeader != null){
+			doTranslate(frames, fastaHeader, sequence.toString().getBytes(),
+					output, includeOriginalFASTA, lineSize);
+		}
+		
+		srStream.close();
+	}
+	
+	public void sixFrameTranslate(String user, int [] frames, String inputFile, 
+			String outputFile, boolean includeOriginalFASTA, int lineSize) throws RepositoryException, 
+			URISyntaxException, StorageException, IllegalArgumentException, IOException{
+		StoredResource outFile = getAccess().createNewResource(user, outputFile);
+		OutputStream out = outFile.write();
+		sixFrameTranslate(user, frames, inputFile, out, includeOriginalFASTA, lineSize);
+		out.close();
+	}
+	
+	
+	
+	
+	protected static final String NEW_LINE = String.format("%n");
+	
+	protected void doTranslate(int[] frames, String fastaHeader, byte[] bytes,
+			OutputStream output, boolean includeOriginalFASTA, int lineSize) throws IOException {
+		if(includeOriginalFASTA){
+			writeFASTA(fastaHeader, bytes, bytes.length, lineSize, output);
+		}
+		byte [] outBuffer = new byte[bytes.length/3 +1];
+		
+		
+		for(int frame: frames){
+			int size = translateOnFrame(frame, bytes, outBuffer);
+			writeFASTA(getFASTAHeader(fastaHeader, frame), outBuffer, size, lineSize, output);
+		}
+	}
+
+	protected String getFASTAHeader(String originalHeader, int frame){
+		return String.format("%s|frame=%d", originalHeader, frame);
+	}
+	
+	
+	protected void writeFASTA(String fastaHeader, byte[] bytes, int writeSize, int lineSize, OutputStream out) throws IOException {
+		out.write(fastaHeader.getBytes());
+		out.write(NEW_LINE.getBytes());
+		lineSize = lineSize > 0 ? lineSize : writeSize;
+		for(int i = 0; i < bytes.length; i+=lineSize){
+			out.write(bytes, i, lineSize);
+			out.write(NEW_LINE.getBytes());
+		}
+		out.flush();
+	}
+
+	public static void main(String[] args) {
+		String NUC = 
+				"ACCATAGATACTGACTGTACTGACGTACCATAGATACTGACTGTACTGACGTA" +
+				"CCATAGATATAGATACTGACTGTACTGACGTACCATAGATACTGACTGTACTG" +
+				"ACGTACCATAGATACTCATAGATACTGACTGTACTGACGTACCATAGATACTG" +
+				"ACTGTACTGACGTACCATAGATACTGACGACTGTATGTACTGACGT";
+		String REV = new StringBuffer(NUC).reverse().toString();
+		
+		byte [] out = new byte[NUC.length()/3+1];
+		SixFrameTranslation st = new SixFrameTranslation(null);
+		
+		st.translate(NUC.getBytes(), 0, NUC.length(), out);
+		System.out.println(new String(out));
+		System.out.println("------------------------");
+		
+		st.translate(REV.getBytes(), 0, REV.length(), out);
+		System.out.println(new String(out));
+		System.out.println("------------------------");
+		
+		st.translateReverseFrame(NUC.getBytes(), 0, NUC.length(), out);
+		System.out.println(new String(out));
+	}
+	
+	
+	/*
 	public static void main(String[] args) {
 		SixFrameTranslation sft = new SixFrameTranslation(null);
 		int IN_SIZE = 384*1024;
@@ -101,7 +243,7 @@ public class SixFrameTranslation extends BaseGenomeService{
 		
 		long start = System.currentTimeMillis();
 		for(int i = 0; i < ITERATIONS; i++){
-			sft.translate(input, output);
+			sft.translate(input,0, input.length, output);
 		}
 		long end = System.currentTimeMillis();
 		
@@ -117,5 +259,5 @@ public class SixFrameTranslation extends BaseGenomeService{
 		System.out.println(totalSecs);
 		System.out.println((totalSecs/86400));
 	}
-	
+	*/
 }
