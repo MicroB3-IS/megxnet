906927fe9c72b2c5b9589d569a397bba6dbdab9b
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-11-29 16:33:00 +0000

    ESA deployment tag 1.5.3

---
+++ megx.net/tags/esa/1.5.3/net.megx.ui.table/src/net/megx/ui/table/json/TableDataResponse.java
@@ -0,0 +1,52 @@
+package net.megx.ui.table.json;
+
+import java.util.List;
+
+public class TableDataResponse<T> {
+	private Integer iTotalRecords;
+	private Integer iTotalDisplayRecords;
+	private String sEcho;
+	private String sColumns;
+
+	private List<T> data;
+
+	public Integer getiTotalRecords() {
+		return iTotalRecords;
+	}
+
+	public void setiTotalRecords(Integer iTotalRecords) {
+		this.iTotalRecords = iTotalRecords;
+	}
+
+	public Integer getiTotalDisplayRecords() {
+		return iTotalDisplayRecords;
+	}
+
+	public void setiTotalDisplayRecords(Integer iTotalDisplayRecords) {
+		this.iTotalDisplayRecords = iTotalDisplayRecords;
+	}
+
+	public String getsEcho() {
+		return sEcho;
+	}
+
+	public void setsEcho(String sEcho) {
+		this.sEcho = sEcho;
+	}
+
+	public String getsColumns() {
+		return sColumns;
+	}
+
+	public void setsColumns(String sColumns) {
+		this.sColumns = sColumns;
+	}
+
+	public List<T> getData() {
+		return data;
+	}
+
+	public void setData(List<T> data) {
+		this.data = data;
+	}
+}
