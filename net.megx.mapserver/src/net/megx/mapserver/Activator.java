ea8cb0e25d0eef1894ca526d86a22cef74ec4315
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-12-04 16:02:32 +0000

    cleanup mapserver proxy servlet

--- net.megx.mapserver/src/net/megx/mapserver/Activator.java
+++ net.megx.mapserver/src/net/megx/mapserver/Activator.java
@@ -4,27 +4,30 @@ import java.util.Hashtable;
 
 import javax.servlet.Servlet;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
-
+	private static final Log log = LogFactory.getLog(Activator.class);
+	
 	// local root url listener
-	private static final String PROP_MAPSERVER_ROOT = "mapserver.root"; 
+	private static final String PROP_MAPSERVER_ROOT = "mapserver.local.root"; 
 	
 	// remote mapserver basic url
-	private static final String PROP_MAPSERVER_PROXY = "mapserver.proxy"; // remove
+	private static final String PROP_MAPSERVER_URL = "mapserver.remote.url";
 	
-	// remote mapserver path
-	private static final String PROP_MAPSERVER_PROXY_PATH = "mapserver.proxy.path";
+	//map file that goes as a paameter to maprser.remote.url
+	private static final String PROP_MAPSERVER_MAPFILE = "mapserver.map.file";
 	
 	
 	private static final String DEFAULT_MAPSERVER_ROOT = "/wms";
 	
-	private static final String DEFAULT_MAPSERVER_PROXY = "http://www.megx.net";
+	private static final String DEFAULT_MAPSERVER_URL = "";
 	
-	private static final String DEFAULT_MAPSERVER_PROXY_PATH = "/wms/gms";
+	private static final String DEFAULT_MAPSERVER_MAPFILE = "";
 	
 	
 	private ServiceRegistration servletContainerRegRef;
@@ -36,9 +39,15 @@ public class Activator implements BundleActivator {
 		Hashtable<String, String> props = new Hashtable<String, String>();
 		props.put("alias", serveltRoot);
 		
-		String mapserverProxy = System.getProperty(PROP_MAPSERVER_PROXY, DEFAULT_MAPSERVER_PROXY);
-		String mapserverProxyPath = System.getProperty(PROP_MAPSERVER_PROXY_PATH, DEFAULT_MAPSERVER_PROXY_PATH);
-		ProxyServlet proxyServlet = new ProxyServlet(mapserverProxy, mapserverProxyPath);
+		log.debug("Registering mapserver ProxyServlet under mapserver.root = " + serveltRoot);
+		
+		String mapserverUrl = System.getProperty(PROP_MAPSERVER_URL, DEFAULT_MAPSERVER_URL);
+		String mapserverMapfile = System.getProperty(PROP_MAPSERVER_MAPFILE, DEFAULT_MAPSERVER_MAPFILE);
+		
+		log.debug("- mapserverURL = " + mapserverUrl);
+		log.debug("- mapserverMapfile = " + mapserverMapfile);
+		
+		MapserverProxyServlet proxyServlet = new MapserverProxyServlet(mapserverUrl, mapserverMapfile);
 		servletContainerRegRef = bundleContext.registerService(Servlet.class.getName(), proxyServlet, props);
 	}
 
