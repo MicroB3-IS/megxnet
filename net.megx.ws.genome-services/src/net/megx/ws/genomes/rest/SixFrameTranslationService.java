d36edf6205d22ca36343b515397ac07bc8f306db
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-12-07 10:51:20 +0000

    Six frame translation services error handling. Added README for linux curl call (desktop test to the services)

--- net.megx.ws.genome-services/src/net/megx/ws/genomes/rest/SixFrameTranslationService.java
+++ net.megx.ws.genome-services/src/net/megx/ws/genomes/rest/SixFrameTranslationService.java
@@ -3,6 +3,11 @@ package net.megx.ws.genomes.rest;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.NoSuchNodeTypeException;
+import javax.jcr.security.AccessControlException;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,9 +17,13 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
 
+import net.megx.storage.ResourceAccessException;
+import net.megx.storage.StorageException;
+import net.megx.storage.StorageSecuirtyException;
 import net.megx.ws.core.BaseRestService;
 import net.megx.ws.core.Result;
 import net.megx.ws.genomes.SixFrameTranslation;
@@ -39,7 +48,8 @@ public class SixFrameTranslationService extends BaseRestService{
 			@QueryParam("outfile") final String outFile,
 			@Context HttpServletRequest request){
 		
-		final String username = request.getUserPrincipal() != null ? request.getUserPrincipal().getName() : null;
+		final String username = request.getUserPrincipal() != null ? 
+					request.getUserPrincipal().getName() : null;
 		if(username == null){
 			return Response.status(Response.Status.FORBIDDEN).build();
 		}
@@ -54,16 +64,44 @@ public class SixFrameTranslationService extends BaseRestService{
 				
 				try {
 					if(outFile != null){
-						sixFrameTranslation.sixFrameTranslate(username, ALL_FRAMES, infile, outFile, true, LINE_SIZE);
+						sixFrameTranslation.sixFrameTranslate(username, ALL_FRAMES, 
+								infile, outFile, true, LINE_SIZE);
 						Result<String> result = new Result<String>(outFile);
 						output.write(toJSON(result).getBytes());
 					}else{
-						sixFrameTranslation.sixFrameTranslate(username, ALL_FRAMES, infile, output, true, LINE_SIZE);
+						sixFrameTranslation.sixFrameTranslate(username, ALL_FRAMES, 
+								infile, output, true, LINE_SIZE);
 					}
 				} catch (IOException e) {
+					log.error(e);
 					throw e;
-				} catch (Exception e){
-					throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
+				} catch (AccessControlException e) {
+					log.error(e);
+					throw new WebApplicationException(e, Status.UNAUTHORIZED);
+				} catch(ItemNotFoundException e){ 
+					log.error(e);
+					throw new WebApplicationException(e, Status.NOT_FOUND);
+				} catch(PathNotFoundException e){ 
+					log.error(e);
+					throw new WebApplicationException(e, Status.NOT_FOUND);
+				} catch (NoSuchNodeTypeException e) {
+					log.error(e);
+					throw new WebApplicationException(e, Status.NOT_FOUND);
+				} catch (RepositoryException e) {
+					log.error(e);
+					throw new WebApplicationException(e);
+				}  catch (ResourceAccessException e) {
+					log.error(e);
+					throw new WebApplicationException(e, Status.NOT_FOUND);
+				} catch (StorageSecuirtyException e) {
+					log.error(e);
+					throw new WebApplicationException(e, Status.UNAUTHORIZED);
+				} catch (StorageException e) {
+					log.error(e);
+					throw new WebApplicationException(e);
+				} catch (Exception e) {
+					log.error(e);
+					throw new WebApplicationException(e);
 				}
 				
 			}
