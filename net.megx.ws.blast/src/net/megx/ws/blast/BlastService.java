4d26c60c9e60addc29ccf1bf790327c041c03ece
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-12-11 18:31:31 +0000

    put blast ws bundle in main pom.xml addnig javadocs

--- net.megx.ws.blast/src/net/megx/ws/blast/BlastService.java
+++ net.megx.ws.blast/src/net/megx/ws/blast/BlastService.java
@@ -7,8 +7,42 @@ import net.megx.ws.blast.uidomain.BlastJob;
 import net.megx.ws.blast.uidomain.BlastMatchingSequence;
 
 public interface BlastService {
+	/**
+	 * Called from UI to get blast job status by jobId
+	 * Recognized statuses are 
+	 * 	running
+	 *  completed
+	 *  
+	 * @param jobId
+	 * @return
+	 */
 	public String getBlastJobStatus(String jobId);
+	
+	/**
+	 * Called by UI to get blast job info
+	 * 
+	 * @param jobId
+	 * @return
+	 */
 	public BlastJob getBlastJob(String jobId);
+	
+	/**
+	 * Return matching sequences for blast job
+	 * 
+	 * @param jobId
+	 * @return
+	 */
 	public List<BlastMatchingSequence> getMatchingSequence(String jobId);
+	
+	/**
+	 * Start new blast job, return blast job id
+	 * 
+	 * @param seq, input stream, can be either from file of manually entered on UI in textbox
+	 * 
+	 * @param blastDb
+	 * 
+	 * @param evalueCutoff
+	 * @return
+	 */
 	public String runBlastJob(InputStream seq, String blastDb, String evalueCutoff);
 }
