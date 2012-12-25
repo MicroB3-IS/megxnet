0c90367c9b155f80ccad1e6ff8a425a95a635076
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2012-12-25 15:50:46 +0000

    Earth Sampling App, server, 0.3.3-1

---
+++ megx.net/tags/esa/0.3.3-1/net.megx.security.filter/src/main/java/net/megx/security/filter/impl/RestServicesExceptionHandler.java
@@ -0,0 +1,78 @@
+package net.megx.security.filter.impl;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.megx.security.auth.SecurityException;
+import net.megx.security.auth.web.WebUtils;
+import net.megx.ws.core.Result;
+
+import org.json.JSONArray;
+
+import com.google.gson.Gson;
+
+public class RestServicesExceptionHandler extends BaseExceptionHandler{
+
+	private String [] resourcesPatterns;
+	private Gson gson = new Gson();
+	
+	@Override
+	public boolean handleException(Exception e, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		if(resourcesPatterns != null){
+			String path = WebUtils.getRequestPath(request, false);
+			for(String pattern: resourcesPatterns){
+				if(path.matches(pattern)){
+					return doHandleException(e, request, response);
+				}
+			}
+		}
+		return true;
+	}
+	
+	
+	protected boolean doHandleException(Exception e, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		if(e instanceof SecurityException){
+			if(!response.isCommitted()){
+				SecurityException se = (SecurityException)e;
+				response.setStatus(se.getResponseCode());
+				Result<SecurityException> result = new Result<SecurityException>(true, se.getMessage(), "security-exception");
+				//result.setData(se);
+				response.getWriter().write(gson.toJson(result));
+				return false;
+			}
+		}else{
+			if(!response.isCommitted()){
+				response.setStatus(500);
+				Result<Object> result = new Result<Object>(true, e.getMessage(), "server-error");
+				response.getWriter().write(gson.toJson(result));
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public void init() {
+		JSONArray pts = config.optJSONArray("patterns");
+		if(pts != null){
+			List<String> patterns = new LinkedList<String>();
+			for(int i = 0; i < pts.length(); i++){
+				String pattern = pts.optString(i);
+				if(pattern != null && !"".equals(pattern.trim()) ){
+					patterns.add(pattern.trim());
+				}
+			}
+			if(patterns.size() > 0){
+				resourcesPatterns = patterns.toArray(new String []{});
+			}
+		}
+	}
+
+}
