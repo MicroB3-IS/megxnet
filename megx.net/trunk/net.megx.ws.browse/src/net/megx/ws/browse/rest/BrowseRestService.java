c87d39ea267c467eb5aa0e0c54e8c43b8e335cc3
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-01-09 12:23:52 +0000

    further implementation of genomes tables...

--- megx.net/trunk/net.megx.ws.browse/src/net/megx/ws/browse/rest/BrowseRestService.java
+++ megx.net/trunk/net.megx.ws.browse/src/net/megx/ws/browse/rest/BrowseRestService.java
@@ -11,9 +11,15 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import net.megx.megdb.browse.BrowseService;
+import net.megx.model.browse.GenomesRow;
 import net.megx.model.browse.MetagenomesRow;
+import net.megx.model.browse.PhagesRow;
 import net.megx.ui.table.json.TableDataRequest;
 import net.megx.ui.table.json.TableDataResponse;
+import net.megx.ws.browse.rest.tables.GenomesUiTblRow;
+import net.megx.ws.browse.rest.tables.MetagenomesUiTblRow;
+import net.megx.ws.browse.rest.tables.PhagesUiTblRow;
+import net.megx.ws.browse.rest.utils.ConvUtils;
 import net.megx.ws.core.BaseRestService;
 import net.megx.ws.core.Result;
 
@@ -36,13 +42,47 @@ public class BrowseRestService extends BaseRestService {
 	}
 	
 	@GET
+	@Path("genomesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String genomesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<GenomesRow> genomesList = browseService.getGenomes();
+			List<GenomesUiTblRow> list = ConvUtils.dbGenomesToUiList(genomesList);
+			TableDataResponse<GenomesUiTblRow> data = new TableDataResponse<GenomesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
+	@Path("phagesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String phagesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<PhagesRow> phagesList = browseService.getPhages();
+			List<PhagesUiTblRow> list = ConvUtils.dbPhagesToUiList(phagesList);
+			TableDataResponse<PhagesUiTblRow> data = new TableDataResponse<PhagesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
 	@Path("metagenomesTable")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String metagenomesTable(@QueryParam("data") String reqData) {
 		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
 		try {
 			List<MetagenomesRow> metagenomesList = browseService.getMetagenomes();
-			List<MetagenomesUiTblRow> list = ConvUtils.toUiTableList(metagenomesList);
+			List<MetagenomesUiTblRow> list = ConvUtils.dbMetagenomesToUiList(metagenomesList);
 			TableDataResponse<MetagenomesUiTblRow> data = new TableDataResponse<MetagenomesUiTblRow>();
 			data.setData(list);
 			return toJSON(data);
