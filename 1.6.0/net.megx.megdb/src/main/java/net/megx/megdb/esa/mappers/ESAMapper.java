58f2e4bdf3958e47c389c0f9584ed7cef3286ac7
pjonoski <pjonoski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-01-11 16:19:37 +0000

    ESA Server API version 1.6.0 (release)

---
+++ 1.6.0/net.megx.megdb/src/main/java/net/megx/megdb/esa/mappers/ESAMapper.java
@@ -0,0 +1,60 @@
+package net.megx.megdb.esa.mappers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.megx.model.esa.Sample;
+import net.megx.model.esa.SamplePhoto;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface ESAMapper {
+	public Sample getSample(String id);
+	public List<Sample> getSamples(String collectorId);
+	
+	/**
+	 * Adds entry to the samples table
+	 * @param sample
+	 */
+	public void addSample(Sample sample);
+	
+	/**
+	 * Adds entry into the photos table
+	 * @param photo
+	 */
+	public void addPhoto(
+			@Param("photo")   SamplePhoto photo, 
+			@Param("sampleId") String sampleId);
+	
+	/**
+	 * Generally used when the photo data is received
+	 * @param photo
+	 */
+	public int updatePhoto(SamplePhoto photo);
+	
+	public List<SamplePhoto> getPhotosForSample(String sampleId);
+	
+	public void removeSample(String id);
+	
+	public List<Map<String, String>> getConfiguration(String category);
+	public void storeConfiguration(
+			@Param("category") String category, 
+			@Param("config")   Set<Map.Entry<String, String>> config);
+	
+	public String getConfigValue(
+			@Param("category") String category, 
+			@Param("name")    String name);
+	
+	
+	public void storeConfigValue(
+			@Param("category") String category, 
+			@Param("name")     String name,
+			@Param("value")    String value);
+	
+	public void clearConfigValue(
+			@Param("category") String category, 
+			@Param("name")     String name);
+	
+	public void clearConfig(String category);
+}
---
+++ 1.6.0/net.megx.ws.browse/src/net/megx/ws/browse/rest/BrowseRestService.java
@@ -0,0 +1,134 @@
+package net.megx.ws.browse.rest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import net.megx.megdb.browse.BrowseService;
+import net.megx.model.browse.GenomesRow;
+import net.megx.model.browse.MarkerGenesRow;
+import net.megx.model.browse.MetagenomesRow;
+import net.megx.model.browse.PhagesRow;
+import net.megx.model.browse.SamplingSitesRow;
+import net.megx.ui.table.json.TableDataRequest;
+import net.megx.ui.table.json.TableDataResponse;
+import net.megx.ws.browse.rest.tables.GenomesUiTblRow;
+import net.megx.ws.browse.rest.tables.MarkerGenesUiTblRow;
+import net.megx.ws.browse.rest.tables.MetagenomesUiTblRow;
+import net.megx.ws.browse.rest.tables.PhagesUiTblRow;
+import net.megx.ws.browse.rest.tables.SamplingSitesUiTblRow;
+import net.megx.ws.browse.rest.utils.ConvUtils;
+import net.megx.ws.core.BaseRestService;
+import net.megx.ws.core.Result;
+
+@Path("browse")
+public class BrowseRestService extends BaseRestService {
+
+	private BrowseService browseService;
+
+	public BrowseRestService(BrowseService browseService) {
+		this.browseService = browseService;
+	}
+	
+	@GET
+	@Path("ping")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ping() {
+		Map<String, String> json = new HashMap<String, String>();
+		json.put("ping", "pong");
+		return toJSON(json);
+	}
+	
+	@GET
+	@Path("genomesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String genomesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<GenomesRow> genomesList = browseService.getGenomes();
+			List<GenomesUiTblRow> list = ConvUtils.dbGenomesToUiList(genomesList);
+			TableDataResponse<GenomesUiTblRow> data = new TableDataResponse<GenomesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
+	@Path("phagesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String phagesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<PhagesRow> phagesList = browseService.getPhages();
+			List<PhagesUiTblRow> list = ConvUtils.dbPhagesToUiList(phagesList);
+			TableDataResponse<PhagesUiTblRow> data = new TableDataResponse<PhagesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
+	@Path("metagenomesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String metagenomesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<MetagenomesRow> metagenomesList = browseService.getMetagenomes();
+			List<MetagenomesUiTblRow> list = ConvUtils.dbMetagenomesToUiList(metagenomesList);
+			TableDataResponse<MetagenomesUiTblRow> data = new TableDataResponse<MetagenomesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
+	@Path("markerGenesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String markerGenesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<MarkerGenesRow> markerGenesList = browseService.getMarkerGenes();
+			List<MarkerGenesUiTblRow> list = ConvUtils.dbMarkerGenesToUiList(markerGenesList);
+			TableDataResponse<MarkerGenesUiTblRow> data = new TableDataResponse<MarkerGenesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	@GET
+	@Path("samplingSitesTable")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String samplingSitesTable(@QueryParam("data") String reqData) {
+		TableDataRequest req = fromJSON(reqData, TableDataRequest.class);
+		try {
+			List<SamplingSitesRow> samplingSitesList = browseService.getSamplingSites();
+			List<SamplingSitesUiTblRow> list = ConvUtils.dbSamplingSitesToUiList(samplingSitesList);
+			TableDataResponse<SamplingSitesUiTblRow> data = new TableDataResponse<SamplingSitesUiTblRow>();
+			data.setData(list);
+			return toJSON(data);
+		} catch (Exception e) {
+			Result<Map<String, Object>> r = this.handleException(e);
+			return toJSON(r);
+		}
+	}
+	
+	
+}
