b1e433e7ae07f02813d56339aeb1649218847d25
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-01-17 09:56:21 +0000

    creating tag 0.3.5

---
+++ megx.net/tags/0.3.5/net.megx.megdb/src/main/java/net/megx/megdb/BaseMegdbService.java
@@ -0,0 +1,154 @@
+package net.megx.megdb;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * Base MegDB service. This class is intended to be sub-classed by concrete
+ * implementation for specific DataBase access object.
+ * It provides helper methods for getting MyBatis mappers and executing 
+ * database queries in transactions or in sessions.
+ * <p>
+ * A concrete example would be as follows:
+ * <code>
+ * <pre>
+  public class UserDBService extends BaseMegdbService{
+  
+  		public User getUserByUsername(final String username) throws Exception{
+  			return doInSession(new BaseMegdbService.DBTask<UserMapper,User>(){
+  				public User execute(UserMapper mapper) throws Exception{
+  					return mapper.selectUserByUsername(username);
+ 				}
+  			}, UserMapper.class);
+  		}
+  		
+  
+  		public void removeUser(final String username) throws Exception{
+  			doInTrasaction(new BaseMegdbService.DBTask<UserMapper,Object>(){
+  				public Object execute(UserMapper mapper) throws Exception{
+  					mapper.clearRoleFor(username);
+  					mapper.clearConsumersFor(username);
+  					mapper.removeUser(username);
+  					return null;
+ 				}
+  			}, UserMapper.class);
+ 		}
+  		
+  }
+  </pre>
+ * </code>
+ * 
+ * As seen from the example, UserDBService implements two methods - query for user and removal of user.
+ * A precondition here is that the UserMapper needs to exist. This is the MyBatis mapper interface, and User is
+ * the DTO for database access.
+ * 
+ * The first method performs a simple query and this would be logical place not to use transaction.
+ * The second method uses the helper "doInTransation", which explicitly sets a transaction for the "execute" method.
+ * @author pavle
+ *
+ */
+public class BaseMegdbService {
+	
+	/**
+	 * Represents a DB query task to be executed.<br/>
+	 * The interface is <code>protected</code> and is intended to be used only by the
+	 * subclasses of {@link BaseMegdbService}.
+	 * @author pavle
+	 *
+	 * @param <M> is the MyBatis mapper interface
+	 * @param <R> is the type of the result object that is to be returned.
+	 */
+	protected interface Task<M,R>{
+		/**
+		 * Execute the database task.
+		 * This method is called after the SQL session is set and the Task is ready to execute.
+		 * @param mapper is the MyBatis mapper instance.
+		 * @return the result from the task
+		 * @throws Exception
+		 */
+		public R execute(M mapper) throws Exception;
+		
+		/**
+		 * Sets the MyBatis SQLSession instance available for this Task.<br/>
+		 * The SQL session needs to be available, if some other mapper is needed at runtime
+		 * and must be provided in the same session.
+		 * @param session
+		 */
+		public void setSession(SqlSession session);
+	}
+	
+	/**
+	 * Is a basic implementation of {@link Task}. This class implemnts the boilerplate code around
+	 * the SQL session setup.
+	 * 
+	 * @author pavle
+	 *
+	 * @param <M>
+	 * @param <R>
+	 */
+	public static abstract class DBTask<M,R> implements Task<M, R>{
+		protected SqlSession session;
+		public void setSession(SqlSession session){
+			this.session = session;
+		}
+	}
+	
+	
+	protected Log log = LogFactory.getLog(getClass());
+	
+	
+	protected SqlSessionFactory sessionFactory;
+	
+	
+	public void setSqlSessionFactory(SqlSessionFactory factory){
+		sessionFactory = factory;
+	}
+	
+	
+	protected <M,R> R  doInTransaction(Task<M, R> task, Class<? extends M> mapperClass) throws Exception{
+		log.debug("Executing Task in transaction...");
+		SqlSession session = sessionFactory.openSession();
+		log.debug("Session is opened.");
+		task.setSession(session);
+		M mapper = session.getMapper(mapperClass);
+		log.debug("Mapper instance created...");
+		try {
+			log.debug("Executing task...");
+			R result = task.execute(mapper);
+			log.debug("Task executed. Committing transaction...");
+			session.commit();
+			log.debug("Transaction committed.");
+			return result;
+		} catch (Exception e) {
+			log.debug("An error occured. The transaction will be rolled back. Exception: ", e);
+			session.rollback(true);
+			log.debug("Transaction has been rolled back.");
+			throw e;
+		} finally{
+			session.close();
+			log.debug("Session closed.");
+		}
+	}
+	protected <M,R> R  doInSession(Task<M, R> task, Class<? extends M> mapperClass) throws Exception{
+		log.debug("Executing Task in session...");
+		SqlSession session = sessionFactory.openSession();
+		log.debug("Session is opened.");
+		task.setSession(session);
+		M mapper = session.getMapper(mapperClass);
+		log.debug("Mapper instance created...");
+		try {
+			log.debug("Executing task...");
+			R result = task.execute(mapper);
+			log.debug("Task executed.");
+			return result;
+		} catch (Exception e) {
+			log.debug("An error occured: ", e);
+			throw e;
+		} finally{
+			session.close();
+			log.debug("Session closed.");
+		}
+	}
+}
---
+++ megx.net/tags/0.3.5/net.megx.megdb/src/main/java/net/megx/security/auth/services/db/UserMapper.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2011 Max Planck Institute for Marine Microbiology 
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+package net.megx.security.auth.services.db;
+
+import java.util.List;
+
+import net.megx.security.auth.model.Permission;
+import net.megx.security.auth.model.Role;
+import net.megx.security.auth.model.User;
+import net.megx.security.auth.model.UserVerification;
+
+import org.apache.ibatis.annotations.Param;
+
+
+public interface UserMapper {
+	
+
+	public User getUserByUserId(String userId) throws Exception;
+
+	public User getUserByConsumerKey(String key) throws Exception;
+
+	public User getUserForUsernameAndPassword(
+			@Param("username") String username,
+			@Param("password") String password) throws Exception;
+
+	//public User getUserByUniqueIdentifier(String uniqueIdentifier)
+	//		throws Exception;
+	
+	public void addUser(User user) throws Exception;
+	public void updateUser(User user) throws Exception;
+	public void removeUser(String login) throws Exception;
+	
+	
+	public void revokeRole(@Param("login")String login, @Param("role")String role) throws Exception;
+	public void grantRole(@Param("login")String login, @Param("role")String role) throws Exception;
+	public int createRole(Role role) throws Exception;
+	
+	public List<Role> getAllRoles() throws Exception;
+	
+	public List<Role> getAllRolesPaginated(
+			@Param("offset") int offset, 
+			@Param("limit")  int limit) throws Exception;
+	
+	public int getRolesCount() throws Exception;
+	
+	public List<Role> getRolesForNames(@Param("names")List<String> names) throws Exception;
+	
+	
+	public void removeRole(String role) throws Exception;
+	public Role getRole(String role) throws Exception;
+	public List<User> getUsers() throws Exception; //FIXME: pagination
+	
+	public List<User> getUsersPaginated(@Param("offset") int offset,
+											@Param("limit") int limit) throws Exception;
+	public int countUsers() throws Exception;
+	
+	public List<User> getUsersWithRole(String role) throws Exception;
+	public List<User> filterUsers(
+			@Param("username")String username, 
+			@Param("maxResults") int maxResults) throws Exception;
+	
+	public void createVerification(UserVerification verification) throws Exception;
+	public UserVerification getVerification(
+			@Param("verificationValue")String value, 
+			@Param("ttl") long ttl) throws Exception;
+	
+	public void deleteVerification(String verification) throws Exception;
+	public void cleanupVerifications(long ttl) throws Exception;
+	
+	
+	public void updateRole(
+			@Param("oldLabel") String oldLabel,
+			@Param("role") Role role) throws Exception;
+	public void grantPermission(
+			@Param("role") String role, 
+			@Param("permission") String permission) throws Exception;
+	public void revokePermission(
+			@Param("role") String role, 
+			@Param("permission") String permission) throws Exception;
+	
+	public void removePermission(String permission) throws Exception;
+	
+	public List<Permission> getAllPermissions() throws Exception;
+	
+	// users pagination
+	public List<User> filterUsersWithRole(
+			@Param("conditions") List<FilterCondition> conditions,
+			@Param("role") String role,
+			@Param("start") int start,
+			@Param("limit") int limit) throws Exception;
+	public int countFilteredResultsWithRole(
+			@Param("conditions") List<FilterCondition> conditions,
+			@Param("role") String role) throws Exception;
+}
