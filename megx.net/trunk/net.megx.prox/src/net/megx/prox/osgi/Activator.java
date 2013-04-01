1b3addc27bf9cfe161b62ae3dad9bb89a041cbc2
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-04-01 14:42:49 +0000

    MB3_IS-224: Proxy to route ajax calls from the ProX app to Pfam and vice verca

--- megx.net/trunk/net.megx.prox/src/net/megx/prox/osgi/Activator.java
+++ megx.net/trunk/net.megx.prox/src/net/megx/prox/osgi/Activator.java
@@ -1,13 +1,19 @@
-package net.megx.prox;
+package net.megx.prox.osgi;
+
+import net.megx.prox.rest.PfamProxy;
 
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
+import org.chon.web.RegUtils;
 
 public class Activator extends ResTplConfiguredActivator {
 
 	@Override
 	protected void registerExtensions(JCRApplication app) {
-		// TODO register extensions
+		PfamProxy pfamProxy = new PfamProxy();
+		RegUtils.reg(getBundleContext(),
+				PfamProxy.class
+						.getName(), pfamProxy, null);
 	}
 
 	@Override
