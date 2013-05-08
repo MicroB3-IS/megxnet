059c36176ac2b59bfffa44de503b8cd4fef03ad0
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-05-08 14:36:11 +0000

    adding GeoPfam service

---
+++ net.megx.megdb/src/main/java/net/megx/megdb/geopfam/mappers/GeoPfamMapper.java
@@ -0,0 +1,9 @@
+package net.megx.megdb.geopfam.mappers;
+
+import java.util.List;
+
+import net.megx.model.geopfam.GeoPfamRow;
+
+public interface GeoPfamMapper {
+	public List<GeoPfamRow> getAll();
+}
