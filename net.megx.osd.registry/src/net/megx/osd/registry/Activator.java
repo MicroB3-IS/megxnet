88fa10f8388dc6ee4bd1f490f66d5178791ae07d
jveljanovski <jveljanovski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-05-31 10:59:56 +0000

    adding net.megx.osd.registry plugin

---
+++ net.megx.osd.registry/src/net/megx/osd/registry/Activator.java
@@ -0,0 +1,18 @@
+package net.megx.osd.registry;
+
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+	@Override
+	protected void registerExtensions(JCRApplication app) {
+		// TODO register extensions
+	}
+
+	@Override
+	protected String getName() {
+		return "net.megx.osd.registry";
+	}
+	
+}
