6f037bef1985723c86cd4b0849bc1b10f2610be2
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-07-15 10:03:21 +0000

    Redirect callback handler to be called after the ajax call is made and the server returns the result

---
+++ megx.net/tags/0.3.13.1/net.megx.megdb/src/main/java/net/megx/megdb/geopfam/impl/DBGeoPfamService.java
@@ -0,0 +1,26 @@
+package net.megx.megdb.geopfam.impl;
+
+import java.util.List;
+
+import net.megx.megdb.BaseMegdbService;
+import net.megx.megdb.geopfam.GeoPfamService;
+import net.megx.megdb.geopfam.mappers.GeoPfamMapper;
+import net.megx.model.geopfam.GeoPfamRow;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DBGeoPfamService extends BaseMegdbService implements GeoPfamService {
+	private static final Log log = LogFactory.getLog(GeoPfamService.class);
+	
+	@Override
+	public List<GeoPfamRow> getByTargetAccession(final String targetAccession) throws Exception {
+		return doInSession(new BaseMegdbService.DBTask<GeoPfamMapper, List<GeoPfamRow>>() {
+			@Override
+			public List<GeoPfamRow> execute(GeoPfamMapper mapper) throws Exception {
+				log.debug("Executing  GeoPfam getByTargetAccession ...");
+				return mapper.getByTargetAccession(targetAccession);
+			}
+		}, GeoPfamMapper.class);
+	}
+}
