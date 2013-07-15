c225129eb3dc7cb11abc4c56b9302b8167012f7c
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-07-15 09:49:03 +0000

    Redirect callback handler to be called after the ajax call is made and the server returns the result

---
+++ megx.net/tags/0.3.13/net.megx.megdb/src/main/java/net/megx/megdb/Activator.java
@@ -0,0 +1,165 @@
+package net.megx.megdb;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import net.megx.utils.PasswordHash;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.chon.cms.core.JCRAppConfgEnabledActivator;
+import org.chon.cms.core.JCRApplication;
+import org.chon.web.RegUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends JCRAppConfgEnabledActivator {
+
+	private static final Log log = LogFactory.getLog(Activator.class);
+	private SqlSessionFactory factory = null;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		Class<?> pgDriver = Class.forName("org.postgresql.Driver");
+		if(pgDriver == null) {
+			throw new ClassNotFoundException("org.postgresql.Driver not found!!!");
+		}
+		// super.start calls initialization from JCRAppConfgEnabledActivator
+		super.start(context);
+	}
+	
+
+	@Override
+	protected void onAppAdded(BundleContext context, JCRApplication app) {
+		//must call super here to copyNonExistingConfigsToDistConfigDir
+		super.onAppAdded(context, app);
+		
+		// config available only when JCRApplication available
+		// this method will be called when JCRApplication is available
+		JSONObject cfg = getConfig();
+		log.debug("Megdb Services starting.");
+		try {
+			factory = buildSQLSessionFactory(cfg);
+			buildDBServices(cfg.getJSONArray("dbServices"), factory);
+
+			setupPasswordHash(cfg);
+
+			log.info("Megdb Services start success.");
+		} catch (Exception e) {
+			log.error("Megdb Services failed to startup: " + e.getMessage(), e);
+			//throw e;
+		}
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+	@Override
+	protected String getName() {
+		return "net.megx.megdb";
+	}
+
+	protected SqlSessionFactory buildSQLSessionFactory(JSONObject chonConfig)
+			throws JSONException, IOException {
+		log.debug("Start building SQLSessionFactory...");
+		SqlSessionFactory factory = null;
+		String myBatisConfigFile = chonConfig.getString("myBatisConfigFile");
+		log.debug("MyBatis config file location: " + myBatisConfigFile);
+		factory = new SqlSessionFactoryBuilder().build(
+				Resources.getResourceAsReader(myBatisConfigFile),
+				getDatabaseProperties(chonConfig));
+		return factory;
+	}
+
+	private Properties getDatabaseProperties(JSONObject config) {
+		Properties properties = new Properties();
+
+		JSONObject dbConfig = config.optJSONObject("dbConfig");
+
+		String[] keys = JSONObject.getNames(dbConfig);
+
+		if (keys != null) {
+			for (String key : keys) {
+				properties.put(key, dbConfig.optString(key));
+			}
+		}
+
+		return properties;
+	}
+
+	protected BaseMegdbService registerBaseDBService(
+			Class<? extends BaseMegdbService> cls, Class<?> registerAs,
+			SqlSessionFactory factory) {
+		try {
+			BaseMegdbService dbService = cls.newInstance();
+			dbService.setSqlSessionFactory(factory);
+			log.debug((String.format(
+					"Registering service instance %s of class (%s) as (%s)",
+					dbService.toString(), cls.getName(), registerAs.getName())));
+			RegUtils.reg(getBundleContext(), registerAs.getName(), dbService,
+					null);
+			return dbService;
+		} catch (InstantiationException e) {
+			log.error(e);
+		} catch (IllegalAccessException e) {
+			log.error(e);
+		}
+		return null;
+	}
+
+	protected void buildDBServices(JSONArray servicesConfig,
+			SqlSessionFactory sessionFactory) throws JSONException,
+			ClassNotFoundException {
+		log.debug(" Building DB Services: ");
+		for (int i = 0; i < servicesConfig.length(); i++) {
+			JSONObject config = servicesConfig.getJSONObject(i);
+			log.debug(" > Building Service: " + config.toString());
+			BaseMegdbService service = buildBaseDBService(config,
+					sessionFactory);
+			if (service == null) {
+				log.error("Failed to build service.");
+			} else {
+				log.debug(" > Service built and ready: " + service);
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected BaseMegdbService buildBaseDBService(JSONObject config,
+			SqlSessionFactory sessionFactory) throws JSONException,
+			ClassNotFoundException {
+		String serviceName = config.getString("name");
+		String implementingClass = config.getString("class");
+		return registerBaseDBService(
+				(Class<? extends BaseMegdbService>) loadClass(implementingClass),
+				loadClass(serviceName), sessionFactory);
+	}
+
+	protected Class<?> loadClass(String className)
+			throws ClassNotFoundException {
+		return Activator.class.getClassLoader().loadClass(className);
+	}
+
+	protected void setupPasswordHash(JSONObject config){
+		JSONObject passwordHashing = config.optJSONObject("passwordHashing");
+		if(passwordHashing != null){
+			int saltBytes = config.optInt("saltBytes", PasswordHash.SALT_BYTES);
+			int hashBytes = config.optInt("hashBytes", PasswordHash.HASH_BYTES);
+			int iterations = config.optInt("PBKDF2iterations", PasswordHash.PBKDF2_ITERATIONS);
+			PasswordHash.SALT_BYTES = saltBytes;
+			PasswordHash.HASH_BYTES = hashBytes;
+			PasswordHash.PBKDF2_ITERATIONS = iterations;
+			if(log.isDebugEnabled())
+				log.debug(String.format("Password Hashing Options: saltBytes=%d, hashBytes=%d, iterations=%d", saltBytes, hashBytes, iterations));
+		}else{
+			log.debug("Password Hashing Options are set to default.");
+		}
+	}
+}
---
+++ megx.net/tags/0.3.13/net.megx.specbyexample.accepttest/src/test/java/net/megx/wod05/WOA05StepDefs.java
@@ -0,0 +1,66 @@
+package net.megx.wod05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.annotation.After;
+import cucumber.annotation.Before;
+import cucumber.annotation.en.And;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+public class WOA05StepDefs 
+{
+	protected WebDriver driver;
+
+	@Before
+	public void setUp() 
+	{
+		driver = new FirefoxDriver();
+	}
+
+	@Given("the user is on WOD05 Extractor Page")
+	public void the_user_is_on_woa05_extractor_page() 
+	{
+	    driver.get("http://www.megx.net/gms/tools/wod.html");
+	}
+
+	@When("he enters \"([^\"]*)\" as latitude")
+	public void he_enters_latitude(String latitude) {
+		driver.findElement(By.id("latitude")).sendKeys(latitude);
+	}
+
+	@And("he enters \"([^\"]*)\" as longitude")
+	public void he_enters_longitude(String longitude) 
+	{
+		driver.findElement(By.id("longitude")).sendKeys(longitude);
+	}
+	
+	@And("he enters \"([^\"]*)\" as depth")
+	public void he_enters_depth(String depth) 
+	{
+		driver.findElement(By.id("depth")).sendKeys(depth);
+	}
+	
+	@And("he submits request by clicking the Calculate button")
+	public void he_submits_request_by_clicking_the_caculate_button()
+	{
+	    driver.findElement(By.name("submit")).click();	    
+	}	
+	
+	@Then("ensure the result is complete")
+	public void ensure_the_result_is_complete() 
+	{
+	    //WebElement message = driver.findElement(By.id("message"));
+		//WebElement message = driver.findElement(By.id("message"));
+		//assertEquals(message.getText(),"msg");		
+	}
+
+	@After
+	public void tearDown() 
+	{
+	    driver.close();
+	}
+}
