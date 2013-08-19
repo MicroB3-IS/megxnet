699105276a3b223d1a52fbc2557b7f6bb9799252
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-08-19 14:27:56 +0000

    made mvn build run cucumber integration tests only

--- megx.net/trunk/net.megx.specbyexample.accepttest/src/test/java/net/megx/wod05/WOA05StepDefs.java
+++ megx.net/trunk/net.megx.specbyexample.accepttest/src/test/java/net/megx/wod05/WOA05StepDefs.java
@@ -4,12 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import cucumber.annotation.After;
-import cucumber.annotation.Before;
-import cucumber.annotation.en.And;
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.Then;
-import cucumber.annotation.en.When;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.*;
 
 public class WOA05StepDefs 
 {
