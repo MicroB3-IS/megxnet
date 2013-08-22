057e2c6cbef5f92fd0599768414fce0d65127a71
akolhoff <akolhoff@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-08-22 11:31:29 +0000

    New StepDefs Classes added

---
+++ net.megx.specbyexample.accepttest/src/test/java/net/megx/osd/registry/DeleteDataStepDefs.java
@@ -0,0 +1,54 @@
+package net.megx.osd.registry;
+
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class DeleteDataStepDefs 
+{
+	 protected WebDriver driver;
+		
+		@Before
+		public void setUp() 
+		{
+			//driver = new FirefoxDriver();
+		}
+		@Given("the administrator is on osd_admin_delete page \"([^\"]*)\"") 
+		public void the_administrator_is_on_the_osd_admin_delete_page(String tail_of_url) throws Throwable
+		{
+		    //driver.get("http://www.megx.net/"+ tail_of_url);		
+			throw new PendingException();
+
+		}
+		@When("he select one datarecord")
+		public void he_select_one_datarecord() throws Throwable
+		{
+			//driver.findElement(By.id("<id>")).click();		
+			throw new PendingException();
+		}
+		
+		@And("he clicks the ok button from an appearing confirmation panel")
+		public void he_click_confirmation_ok() throws Throwable
+		{
+			//driver.findElement(By.id("<id>")).click();		
+			throw new PendingException();
+		}
+		
+		@Then("the datarecord was removed")
+		public void the_datarecord_was_removed() throws Throwable
+		{		    	
+			throw new PendingException();
+		}
+
+		@After
+		public void tearDown() 
+		{
+		    //driver.close();
+		}
+}
---
+++ net.megx.specbyexample.accepttest/src/test/java/net/megx/osd/registry/RegisterNewSamplingSiteStepDefs.java
@@ -0,0 +1,47 @@
+package net.megx.osd.registry;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegisterNewSamplingSiteStepDefs 
+{
+	protected WebDriver driver;
+	
+	@Before
+	public void setUp() 
+	{
+		//driver = new FirefoxDriver();
+	}
+	@Given("the administrator is on the osd_admin_register Page \"([^\"]*)\"") 
+	public void the_administrator_is_on_the_osd_admin_register_page(String tail_of_url) throws Throwable
+	{
+	    //driver.get("http://www.megx.net/"+ tail_of_url);		
+		throw new PendingException();
+
+	}
+
+	@When("he submits data by clicking the save button")
+	public void he_submits_data_by_clicking_the_save_button() throws Throwable
+	{
+		//driver.findElement(By.id("<id>")).click();		
+		throw new PendingException();
+	}
+	@Then("ensure the data were stored by displaying the message \"([^\"]*)\"")
+	public void ensure_the_data_were_stored(String message) throws Throwable
+	{
+	    //WebElement message = driver.findElement(By.id("<id>"));		
+		//assertEquals(message.getText(),"message");		
+		throw new PendingException();
+	}
+
+	@After
+	public void tearDown() 
+	{
+	    //driver.close();
+	}
+}
