30ee3d5c31de16a0dfc46ad051d4c76799ccb3da
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-08-28 16:45:24 +0000

    fixed compile errors

--- megx.net/trunk/net.megx.specbyexample.accepttest/src/test/java/net/megx/wod05/WOA05StepDefs.java
+++ megx.net/trunk/net.megx.specbyexample.accepttest/src/test/java/net/megx/wod05/WOA05StepDefs.java
@@ -8,57 +8,48 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
 
-public class WOA05StepDefs 
-{
-	protected WebDriver driver;
+public class WOA05StepDefs {
+    protected WebDriver driver;
 
-	@Before
-	public void setUp() 
-	{
-		driver = new FirefoxDriver();
-		driver.
-	}
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
 
-	@Given("the user is on WOD05 Extractor Page")
-	public void the_user_is_on_woa05_extractor_page() 
-	{
-	    driver.get("http://www.megx.net/gms/tools/wod.html");
-	}
+    @Given("the user is on WOD05 Extractor Page")
+    public void the_user_is_on_woa05_extractor_page() {
+        driver.get("http://www.megx.net/gms/tools/wod.html");
+    }
 
-	@When("he enters \"([^\"]*)\" as latitude")
-	public void he_enters_latitude(String latitude) {
-		driver.findElement(By.id("latitude")).sendKeys(latitude);
-	}
+    @When("he enters \"([^\"]*)\" as latitude")
+    public void he_enters_latitude(String latitude) {
+        driver.findElement(By.id("latitude")).sendKeys(latitude);
+    }
 
-	@And("he enters \"([^\"]*)\" as longitude")
-	public void he_enters_longitude(String longitude) 
-	{
-		driver.findElement(By.id("longitude")).sendKeys(longitude);
-	}
-	
-	@And("he enters \"([^\"]*)\" as depth")
-	public void he_enters_depth(String depth) 
-	{
-		driver.findElement(By.id("depth")).sendKeys(depth);
-	}
-	
-	@And("he submits request by clicking the Calculate button")
-	public void he_submits_request_by_clicking_the_caculate_button()
-	{
-	    driver.findElement(By.name("submit")).click();	    
-	}	
-	
-	@Then("ensure the result is complete")
-	public void ensure_the_result_is_complete() 
-	{
-	    //WebElement message = driver.findElement(By.id("message"));
-		//WebElement message = driver.findElement(By.id("message"));
-		//assertEquals(message.getText(),"msg");		
-	}
+    @And("he enters \"([^\"]*)\" as longitude")
+    public void he_enters_longitude(String longitude) {
+        driver.findElement(By.id("longitude")).sendKeys(longitude);
+    }
 
-	@After
-	public void tearDown() 
-	{
-	    driver.close();
-	}
+    @And("he enters \"([^\"]*)\" as depth")
+    public void he_enters_depth(String depth) {
+        driver.findElement(By.id("depth")).sendKeys(depth);
+    }
+
+    @And("he submits request by clicking the Calculate button")
+    public void he_submits_request_by_clicking_the_caculate_button() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    @Then("ensure the result is complete")
+    public void ensure_the_result_is_complete() {
+        // WebElement message = driver.findElement(By.id("message"));
+        // WebElement message = driver.findElement(By.id("message"));
+        // assertEquals(message.getText(),"msg");
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
 }
