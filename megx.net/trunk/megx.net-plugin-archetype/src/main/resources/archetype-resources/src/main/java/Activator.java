1efe2660f7f73fe49c17ff6ac2cd491daed96160
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-09-24 18:41:45 +0000

    initial check-in of maven archetype project creating megx.met plugin project structure

---
+++ megx.net/trunk/megx.net-plugin-archetype/src/main/resources/archetype-resources/src/main/java/Activator.java
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Activator extends ${chon_activator} {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    
+
+    @Override
+    protected String getName() {
+        return "${package}.";
+    }
+
+}
