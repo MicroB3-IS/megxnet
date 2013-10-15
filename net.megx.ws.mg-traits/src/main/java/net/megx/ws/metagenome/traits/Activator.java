9ef30b9707a9ce0c30202ab2a1a415d9decd0fc5
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-10-15 16:27:21 +0000

    Initial functionality for MGTraits

--- net.megx.ws.mg-traits/src/main/java/net/megx/ws/metagenome/traits/Activator.java
+++ net.megx.ws.mg-traits/src/main/java/net/megx/ws/metagenome/traits/Activator.java
@@ -1,9 +1,14 @@
 package net.megx.ws.metagenome.traits;
 
+import net.megx.megdb.mgtraits.MGTraitsService;
+import net.megx.utils.OSGIUtils;
+import net.megx.ws.mg.traits.rest.MGTraitsAPI;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
+import org.chon.web.RegUtils;
 
 public class Activator extends ResTplConfiguredActivator {
 
@@ -12,8 +17,21 @@ public class Activator extends ResTplConfiguredActivator {
 
     @Override
     protected void registerExtensions(JCRApplication app) {
-        // TODO Auto-generated method stub
-        
+        log.debug("MGTraitsAPI starting up");
+        OSGIUtils.requestService(MGTraitsService.class.getName(), 
+				getBundleContext(), 
+				new OSGIUtils.OnServiceAvailable<MGTraitsService>() {
+
+			@Override
+			public void serviceAvailable(String name,
+					MGTraitsService service) {
+				log.debug("MGTraitsService received...");
+				MGTraitsAPI api = new MGTraitsAPI(service);
+				RegUtils.reg(getBundleContext(), MGTraitsAPI.class.getName(), api, null);
+				log.debug("MGTraitsAPI started.");
+			}
+			
+		});
     }    
 
     @Override
