087393f1ba275ecb03a96e1649dd4b8967747300
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-12-12 12:36:11 +0000

    continued adding functionality

--- megx.net/trunk/net.megx.chon.template/res/gms-res/js/MegxMapWidget.js
+++ megx.net/trunk/net.megx.chon.template/res/gms-res/js/MegxMapWidget.js
@@ -10,95 +10,102 @@
 
 // load layersets
 
-MegxMapWidget = function(config) {
-    //TODO: check minimum config params 
-    //TODO: check URL  
-	this.init(config);
+MegxMapWidget = function (elem, layerSet, config) {
+  //TODO: check minimum config params
+  //TODO: check URL
+  if (elem || elem === '') {
+    //TODO throw exception or do alert?
+  }
+  if (layerSet || layerSet === '') {
+    //TODO throw exception or do alert?
+  }
+  this.init(elem, layerSet, config);
 }
 
 MegxMapWidget.prototype = {
-	init: function(config) {
-		this.map = new OpenLayers.Map(config.el, {
-			controls : [],
-			numZoomLevels : 16,
-			projection : "EPSG:4326"
-		});
-		
-		this.addMapControls(this.map);
-		
-		console.log("Init map on gmsURL = " + config.gmsURL);
-		
-		this.layers = new MegxMapWidgetLayers({
-			gms_wms_url: config.gmsURL,
-			sam_genomes: 'genome',
-			sam_metagenomes: 'metagenome',
-			sam_phages: 'phage',
-			sam_rrna: '',
-			extent: new OpenLayers.Bounds(-180, -90, 180, 90)
-		});
-		this.map.addLayers(this.layers.getLayersArray());
-		var map = this.map;
-		//will fail without base layer
-        map.zoomToMaxExtent();
-		
-	},
-	
-	addMapControls: function(map) {
-		var navContr = new OpenLayers.Control.Navigation();
-		navContr.setMap(map);
+  
+  var map = this.map;
+  
+  init : function (elem, layerSet, config) {
+    this.map = new OpenLayers.Map(elem, {
+        controls : [],
+        numZoomLevels : 16,
+        projection : "EPSG:4326"
+      });
 
-		map.addControl(navContr);
-		map.addControl(new OpenLayers.Control.ScaleLine());
-		map.addControl(new OpenLayers.Control.MousePosition());
-		map.addControl(new OpenLayers.Control.KeyboardDefaults());
-		
-        var panZoomContr = new OpenLayers.Control.PanZoomBar();
-		panZoomContr.setMap(map);
-		map.addControl(panZoomContr);
-		map.addControl(new OpenLayers.Control.Attribution());
-		
-        var permalinkContr = new OpenLayers.Control.Permalink('permalink');
-		permalinkContr.setMap(map);
-		map.addControl(permalinkContr);
-	},
-	
-	removeControl: function(controlName){
-		var controls = this.map.getControlsByClass(controlName);
-		for(var i = 0; i < controls.length; i++){
-			controls[i].deactivate();
-		}
-	},
-	
-	addLayer: function(layer){
-		this.map.addLayer(layer);
-	},
-	
-	showLayer: function(name) {
-		this.layers.get(name).setVisibility(true);
-	},
+    this.addMapControls(this.map);
+    //console.log("Initialize map on gmsURL = " + config.gmsURL);
 
-	hideLayer: function(name) {	
-		this.layers.get(name).setVisibility(false);
-	},
-	
-	hideAll: function() {
-		var names = this.layers.getLayersNames();
-		for(var i=0; i<names.length; i++) {
-			this.hideLayer(names[i]);
-		}
-	},
+    this.layers = new MegxMapWidgetLayers({
+        gms_wms_url : config.gmsURL,
+        sam_genomes : 'genome',
+        sam_metagenomes : 'metagenome',
+        sam_phages : 'phage',
+        sam_rrna : '',
+        extent : new OpenLayers.Bounds(-180, -90, 180, 90)
+      });
+    this.map.addLayers(this.layers.getLayersArray());
+    
+    //will fail without base layer
+    map.zoomToMaxExtent();
 
-    getName: function() {
-        return 'renzo';
-    },
+  },
 
-	setTopClickable: function(name) {
-		//TODO: set clickable layer, show on top order ...
-	},
-	
-	redraw: function(name){
-		name = name || '';
-		return this.layers.get(name).redraw();
-	}
+  addMapControls : function (map) {
+    var navContr = new OpenLayers.Control.Navigation();
+    navContr.setMap(map);
+
+    map.addControl(navContr);
+    map.addControl(new OpenLayers.Control.ScaleLine());
+    map.addControl(new OpenLayers.Control.MousePosition());
+    map.addControl(new OpenLayers.Control.KeyboardDefaults());
+
+    var panZoomContr = new OpenLayers.Control.PanZoomBar();
+    panZoomContr.setMap(map);
+    map.addControl(panZoomContr);
+    map.addControl(new OpenLayers.Control.Attribution());
+
+    var permalinkContr = new OpenLayers.Control.Permalink('permalink');
+    permalinkContr.setMap(map);
+    map.addControl(permalinkContr);
+  },
+
+  removeControl : function (controlName) {
+    var controls = this.map.getControlsByClass(controlName);
+    for (var i = 0; i < controls.length; i++) {
+      controls[i].deactivate();
+    }
+  },
+
+  addLayer : function (layer) {
+    this.map.addLayer(layer);
+  },
+
+  showLayer : function (name) {
+    this.layers.get(name).setVisibility(true);
+  },
+
+  hideLayer : function (name) {
+    this.layers.get(name).setVisibility(false);
+  },
+
+  hideAll : function () {
+    var names = this.layers.getLayersNames();
+    for (var i = 0; i < names.length; i++) {
+      this.hideLayer(names[i]);
+    }
+  },
+
+  getName : function () {
+    return 'renzo';
+  },
+
+  setTopClickable : function (name) {
+    //TODO: set clickable layer, show on top order ...
+  },
+
+  redraw : function (name) {
+    name = name || '';
+    return this.layers.get(name).redraw();
+  }
 };
-
