8e1fed3d9298c12824fe36949c5a986e9ecbc664
akolhoff <akolhoff@82e9701a-15f3-4962-89cc-9b74a964515e>
2013-12-15 18:52:42 +0000

    minor changes, errors removed

--- megx.net/trunk/net.megx.test.jbehave/dummy.webapp/src/main/java/war/Participant.java
+++ megx.net/trunk/net.megx.test.jbehave/dummy.webapp/src/main/java/war/Participant.java
@@ -2,6 +2,7 @@
 package war;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,12 +12,15 @@ import javax.ws.rs.core.Response;
 @Path("/participant")
 public class Participant {    
     
-    @GET 
+    @POST 
     @Path("new/{participant}")
+    @Produces("text/plain")
     public Response getMessage(@PathParam("participant")String participant) {
-        // in real application store new participant and
+        // in real application:
+    	// data from form, not as parameter
+    	// store new participant and
     	// return message only by successful storage    	
-    	String message = "Saving successful";
-    	return Response.status(200).entity(message).build();
+    	String message = "Saving successful";    	
+    	return Response.status(201).entity(message).build();
     }    
 }
