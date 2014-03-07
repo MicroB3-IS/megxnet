46fa502adc44fdc9a11b1a17eba9fb4b9c342ccf
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-03-07 10:06:48 +0000

    https://colab.mpi-bremen.de/its/browse/MB3_IS-395: Added taxonomic-content service

--- megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/mgtraits/mappers/MGTraitsMapper.java
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/mgtraits/mappers/MGTraitsMapper.java
@@ -8,29 +8,30 @@ import net.megx.model.mgtraits.MGTraitsDNORatio;
 import net.megx.model.mgtraits.MGTraitsJobDetails;
 import net.megx.model.mgtraits.MGTraitsPfam;
 import net.megx.model.mgtraits.MGTraitsResult;
+import net.megx.model.mgtraits.MGTraitsTaxonomy;
 
 import org.apache.ibatis.annotations.Param;
 
 public interface MGTraitsMapper {
 
-    public void insertMGTraitsJob(MGTraitsJobDetails job);
+	public void insertMGTraitsJob(MGTraitsJobDetails job);
 
-    public MGTraitsResult getSimpleTraits(@Param("id") int id);
+	public MGTraitsResult getSimpleTraits(@Param("id") int id);
 
-    public MGTraitsPfam getFunctionTable(@Param("id") int id);
+	public MGTraitsPfam getFunctionTable(@Param("id") int id);
 
-    public MGTraitsAA getAminoAcidContent(@Param("id") int id);
+	public MGTraitsAA getAminoAcidContent(@Param("id") int id);
 
-    public MGTraitsDNORatio getDiNucleotideOddsRatio(@Param("id") int id);
+	public MGTraitsDNORatio getDiNucleotideOddsRatio(@Param("id") int id);
 
-    public MGTraitsJobDetails getJobDetails(
-            @Param("id") int  id);
-    
-    public MGTraitsJobDetails getSuccesfulJob(
-            @Param("id") int  id);
-    
-    public List<MGTraitsJobDetails> getAllFinishedJobs();
-    
-    public List<MGTraitsCodon> getCodonUsage();
+	public MGTraitsJobDetails getJobDetails(@Param("id") int id);
+
+	public MGTraitsJobDetails getSuccesfulJob(@Param("id") int id);
+
+	public List<MGTraitsJobDetails> getAllFinishedJobs();
+
+	public List<MGTraitsCodon> getCodonUsage();
+
+	public List<MGTraitsTaxonomy> getTaxonomyContent();
 
 }
