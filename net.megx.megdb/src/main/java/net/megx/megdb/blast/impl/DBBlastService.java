62f51dfa1b4692b431e5b2826dbb1c4db4676a79
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-03-27 16:55:06 +0000

    Created initial BlastApi services.

--- net.megx.megdb/src/main/java/net/megx/megdb/blast/impl/DBBlastService.java
+++ net.megx.megdb/src/main/java/net/megx/megdb/blast/impl/DBBlastService.java
@@ -1,11 +1,15 @@
 package net.megx.megdb.blast.impl;
 
+import java.util.List;
+
 import net.megx.megdb.BaseMegdbService;
 import net.megx.megdb.blast.BlastService;
 import net.megx.megdb.blast.mappers.BlastMapper;
 import net.megx.megdb.exceptions.DBGeneralFailureException;
 import net.megx.megdb.exceptions.DBNoRecordsException;
 import net.megx.model.blast.BlastHits;
+import net.megx.model.blast.BlastHitsDb;
+import net.megx.model.blast.BlastHitsNeighbours;
 import net.megx.model.blast.BlastJob;
 
 import org.apache.commons.logging.Log;
@@ -60,4 +64,90 @@ public class DBBlastService extends BaseMegdbService implements BlastService {
 		}
 	}
 
+	@Override
+	public BlastJob getBlastJobDetails(final int blastJobId)
+			throws DBGeneralFailureException, DBNoRecordsException {
+		BlastJob result = doInSession(new DBTask<BlastMapper, BlastJob>() {
+			@Override
+			public BlastJob execute(BlastMapper mapper) throws Exception {
+				return mapper.getBlastJobDetails(blastJobId);
+			}
+		}, BlastMapper.class);
+
+		if (result == null) {
+			throw new DBNoRecordsException("Query returned zero results");
+		} else {
+			return result;
+		}
+	}
+
+	@Override
+	public BlastJob getSuccesfulBlastJob(final int id)
+			throws DBGeneralFailureException, DBNoRecordsException {
+		BlastJob result = doInSession(new DBTask<BlastMapper, BlastJob>() {
+			@Override
+			public BlastJob execute(BlastMapper mapper) throws Exception {
+				return mapper.getSuccesfulBlastJob(id);
+			}
+		}, BlastMapper.class);
+
+		if (result == null) {
+			throw new DBNoRecordsException("Query returned zero results");
+		} else {
+			return result;
+		}
+	}
+
+	@Override
+	public List<BlastHitsNeighbours> getNeighbours(final int jid,
+			final String hitId) throws DBGeneralFailureException,
+			DBNoRecordsException {
+		List<BlastHitsNeighbours> result = doInSession(
+				new DBTask<BlastMapper, List<BlastHitsNeighbours>>() {
+					@Override
+					public List<BlastHitsNeighbours> execute(BlastMapper mapper)
+							throws Exception {
+						return mapper.getNeighbours(jid, hitId);
+					}
+				}, BlastMapper.class);
+		if (result.size() == 0) {
+			throw new DBNoRecordsException("Query returned zero results");
+		} else {
+			return result;
+		}
+	}
+
+	@Override
+	public List<BlastHitsDb> getDatabases() throws DBGeneralFailureException,
+			DBNoRecordsException {
+		List<BlastHitsDb> result = doInSession(
+				new DBTask<BlastMapper, List<BlastHitsDb>>() {
+					@Override
+					public List<BlastHitsDb> execute(BlastMapper mapper)
+							throws Exception {
+						return mapper.getDatabases();
+					}
+				}, BlastMapper.class);
+		if (result.size() == 0) {
+			throw new DBNoRecordsException("Query returned zero results");
+		} else {
+			return result;
+		}
+	}
+
+	@Override
+	public BlastJob getResultRaw(final int id)
+			throws DBGeneralFailureException, DBNoRecordsException {
+		BlastJob result = doInSession(new DBTask<BlastMapper, BlastJob>() {
+			@Override
+			public BlastJob execute(BlastMapper mapper) throws Exception {
+				return mapper.getResultRaw(id);
+			}
+		}, BlastMapper.class);
+		if (result == null) {
+			throw new DBNoRecordsException("Query returned zero results");
+		} else {
+			return result;
+		}
+	}
 }
