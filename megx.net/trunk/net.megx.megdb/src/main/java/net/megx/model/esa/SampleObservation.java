51497a00dc7afdbaca9c8508eb3404db88704244
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-05-12 15:01:14 +0000

    https://colab.mpi-bremen.de/its/browse/MB3_IS-499

--- megx.net/trunk/net.megx.megdb/src/main/java/net/megx/model/esa/SampleObservation.java
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/model/esa/SampleObservation.java
@@ -3,55 +3,77 @@ package net.megx.model.esa;
 import java.util.Date;
 
 public class SampleObservation {
-	
+
+	private String id;
 	private String thumbnailId;
 	private String observer;
 	private Date taken;
 	private String sampleName;
 	private String geoRegion;
-	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getThumbnailId() {
 		return thumbnailId;
 	}
+
 	public void setThumbnailId(String thumbnailId) {
 		this.thumbnailId = thumbnailId;
 	}
+
 	public String getObserver() {
 		return observer;
 	}
+
 	public void setObserver(String observer) {
 		this.observer = observer;
 	}
+
 	public Date getTaken() {
 		return taken;
 	}
+
 	public void setTaken(Date taken) {
 		this.taken = taken;
 	}
+
 	public String getSampleName() {
 		return sampleName;
 	}
+
 	public void setSampleName(String sampleName) {
 		this.sampleName = sampleName;
 	}
+
 	public String getGeoRegion() {
 		return geoRegion;
 	}
+
 	public void setGeoRegion(String geoRegion) {
 		this.geoRegion = geoRegion;
 	}
+
 	@Override
 	public String toString() {
-		return "SampleObservation [thumbnailId=" + thumbnailId + ", observer="
-				+ observer + ", taken=" + taken + ", sampleName=" + sampleName
-				+ ", geoRegion=" + geoRegion + "]";
+		return "SampleObservation [id=" + id + ", thumbnailId=" + thumbnailId
+				+ ", observer=" + observer + ", taken=" + taken
+				+ ", sampleName=" + sampleName + ", geoRegion=" + geoRegion
+				+ "]";
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
 				+ ((geoRegion == null) ? 0 : geoRegion.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result
 				+ ((observer == null) ? 0 : observer.hashCode());
 		result = prime * result
@@ -61,6 +83,7 @@ public class SampleObservation {
 				+ ((thumbnailId == null) ? 0 : thumbnailId.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -74,6 +97,11 @@ public class SampleObservation {
 			if (other.geoRegion != null)
 				return false;
 		} else if (!geoRegion.equals(other.geoRegion))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		if (observer == null) {
 			if (other.observer != null)
@@ -97,6 +125,5 @@ public class SampleObservation {
 			return false;
 		return true;
 	}
-	
-	
+
 }
