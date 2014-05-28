fa2557f9be3b4ce42916db08a05de395d89a2165
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-05-28 22:59:30 +0000

    added sample save method, extracted interface

--- net.megx.osd.registry/src/net/megx/osd/registry/rest/OSDRegistryAPI.java
+++ net.megx.osd.registry/src/net/megx/osd/registry/rest/OSDRegistryAPI.java
@@ -1,7 +1,5 @@
 package net.megx.osd.registry.rest;
 
-import java.util.List;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -10,90 +8,38 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import net.megx.megdb.osdregistry.OSDRegistryService;
-import net.megx.model.osdregistry.OSDParticipant;
-import net.megx.osd.registry.rest.util.OSDParticipantDeserializer;
-import net.megx.ui.table.json.TableDataResponse;
-import net.megx.ws.core.BaseRestService;
+public interface OSDRegistryAPI {
 
-import com.google.gson.GsonBuilder;
+    @Path("participants")
+    @GET
+    public abstract String getAllParticipants();
 
-@Path("v1/OSDRegistry/v1.0.0")
-public class OSDRegistryAPI extends BaseRestService{
-	
-	private OSDRegistryService osdRegistryService;
-	
-	public OSDRegistryAPI(OSDRegistryService osdRegistryService){
-		this.osdRegistryService = osdRegistryService;
-		this.gson = new GsonBuilder().registerTypeAdapter(OSDParticipant.class, new OSDParticipantDeserializer())
-		.serializeNulls()
-		.create();
-	}
-	
-	@Path("participants")
-	@GET
-	public String getAllParticipants(){
-		List<OSDParticipant> osdParticipants;
-		try{
-			osdParticipants = osdRegistryService.getOSDParticipants();
-			TableDataResponse<OSDParticipant> resp = new TableDataResponse<OSDParticipant>();
-			resp.setData(osdParticipants);
-			return toJSON(resp);
-		} catch(Exception e){
-			return toJSON(handleException(e));
-		}
-	}
-	
-	@Path("addParticipant")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String saveOSDParticipant(String participant){
-		try{
-			OSDParticipant osdParticipant = gson.fromJson(participant, OSDParticipant.class);
-			osdRegistryService.storeOSDParticipant(osdParticipant);
-			return toJSON(participant);
-					
-		} catch(Exception e){
-			return toJSON(handleException(e));
-		}
-	}
-	
-	@Path("updateParticipant")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String updateOSDParticipant(String participant){
-		try{
-			OSDParticipant osdParticipant = gson.fromJson(participant, OSDParticipant.class);
-			osdRegistryService.updateOSDParticipant(osdParticipant);
-			return toJSON(osdParticipant);
-		} catch(Exception e){
-			return toJSON(handleException(e));
-		}
-	}
-	
-	@Path("deleteParticipant")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String deleteOSDParticipant(@FormParam("id") String id){
-		try{
-			osdRegistryService.deleteOSDParticipant(id);
-			return toJSON(id);
-		} catch(Exception e){
-			return toJSON(handleException(e));
-		}
-	}
-	
-	@Path("getParticipant")
-	@GET
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getParticipant(@QueryParam("id") String id){
-		try{	
-			OSDParticipant participant = osdRegistryService.getParticipant(id);
-			return toJSON(participant);
-		} catch(Exception e){
-			return toJSON(handleException(e));
-		}
-	}
+    @Path("addParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public abstract String saveOSDParticipant(String participant);
+
+    @Path("updateParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public abstract String updateOSDParticipant(String participant);
+
+    @Path("deleteParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public abstract String deleteOSDParticipant(@FormParam("id") String id);
+
+    @Path("getParticipant")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public abstract String getParticipant(@QueryParam("id") String id);
+ 
+    @Path("sample")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public abstract Response saveOSDSample(@FormParam("json") String sample);
+
 }
---
+++ net.megx.osd.registry/src/net/megx/osd/registry/rest/OSDRegistryOAuthImpl.java
@@ -0,0 +1,162 @@
+package net.megx.osd.registry.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import net.megx.megdb.osdregistry.OSDRegistryService;
+import net.megx.model.osdregistry.OSDParticipant;
+import net.megx.osd.registry.rest.util.OSDParticipantDeserializer;
+import net.megx.ui.table.json.TableDataResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Path("v1/OSDRegistry/v1.0.0")
+public class OSDRegistryOAuthImpl implements OSDRegistryAPI {
+
+    private OSDRegistryService osdRegistryService;
+    private Gson gson = new Gson();
+
+    public OSDRegistryOAuthImpl(OSDRegistryService osdRegistryService) {
+        this.osdRegistryService = osdRegistryService;
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(OSDParticipant.class,
+                        new OSDParticipantDeserializer()).serializeNulls()
+                .create();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.megx.osd.registry.rest.OSDRegistryAPI#getAllParticipants()
+     */
+    @Override
+    @Path("participants")
+    @GET
+    public String getAllParticipants() {
+        List<OSDParticipant> osdParticipants;
+        try {
+            osdParticipants = osdRegistryService.getOSDParticipants();
+            TableDataResponse<OSDParticipant> resp = new TableDataResponse<OSDParticipant>();
+            resp.setData(osdParticipants);
+            return gson.toJson(resp);
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.megx.osd.registry.rest.OSDRegistryAPI#saveOSDParticipant(java.lang
+     * .String)
+     */
+    @Override
+    @Path("addParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String saveOSDParticipant(String participant) {
+        try {
+            OSDParticipant osdParticipant = gson.fromJson(participant,
+                    OSDParticipant.class);
+            osdRegistryService.storeOSDParticipant(osdParticipant);
+            return gson.toJson(participant);
+
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.megx.osd.registry.rest.OSDRegistryAPI#updateOSDParticipant(java.lang
+     * .String)
+     */
+    @Override
+    @Path("updateParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String updateOSDParticipant(String participant) {
+        try {
+            OSDParticipant osdParticipant = gson.fromJson(participant,
+                    OSDParticipant.class);
+            osdRegistryService.updateOSDParticipant(osdParticipant);
+            return gson.toJson(osdParticipant);
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.megx.osd.registry.rest.OSDRegistryAPI#deleteOSDParticipant(java.lang
+     * .String)
+     */
+    // TODO change http method to delete
+    @Override
+    @Path("deleteParticipant")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String deleteOSDParticipant(@FormParam("id") String id) {
+        try {
+            osdRegistryService.deleteOSDParticipant(id);
+            return gson.toJson(id);
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.megx.osd.registry.rest.OSDRegistryAPI#getParticipant(java.lang.String
+     * )
+     */
+    @Override
+    @Path("getParticipant")
+    @GET
+    // TODO chage queryparam to pathparam
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getParticipant(@QueryParam("id") String id) {
+        try {
+            // TODO check what if no partipcant, what if DB error
+            OSDParticipant participant = osdRegistryService.getParticipant(id);
+            return gson.toJson(participant);
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+
+    @Override
+    @Path("sample")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response saveOSDSample(@FormParam("json") String sample) {
+        
+        System.out.println(sample);
+        try {
+
+            return Response.status(201).entity("sample saved").build();
+        } catch (Exception e) {
+            throw new WebApplicationException(500);
+        }
+    }
+}
