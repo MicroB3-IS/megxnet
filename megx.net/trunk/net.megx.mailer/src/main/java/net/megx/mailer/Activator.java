a7272e9d6bf8e0b6177a20059aadb4f90239cb13
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-07-09 11:59:02 +0000

    new mailer plugin bare bone structure

---
+++ megx.net/trunk/net.megx.mailer/src/main/java/net/megx/mailer/Activator.java
@@ -0,0 +1,27 @@
+package net.megx.mailer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    
+
+    @Override
+    protected String getName() {
+        return "net.megx.mailer";
+    }
+
+
+
+	@Override
+	protected void registerExtensions(JCRApplication arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
