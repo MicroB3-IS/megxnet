649417e7ed1286c515c6d8433c5849f68ac9867f
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-07-14 15:29:28 +0000

    contact back-end functionality modifications

--- megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/contact/impl/DBContactService.java
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/contact/impl/DBContactService.java
@@ -1,28 +1,27 @@
 package net.megx.megdb.contact.impl;
 
 import net.megx.megdb.BaseMegdbService;
-import net.megx.megdb.BaseMegdbService.DBTask;
 import net.megx.megdb.contact.ContactService;
 import net.megx.megdb.contact.mappers.ContactMapper;
-import net.megx.megdb.esa.mappers.ESAMapper;
+import net.megx.megdb.exceptions.DBGeneralFailureException;
 import net.megx.model.contact.Contact;
-import net.megx.model.esa.Sample;
 
-public class DBContactService extends BaseMegdbService implements ContactService {
+public class DBContactService extends BaseMegdbService implements
+		ContactService {
 
 	@Override
-	public String storeContact(final Contact contact) throws Exception {
-		
+	public String storeContact(final Contact contact)
+			throws DBGeneralFailureException {
+
 		return doInTransaction(new DBTask<ContactMapper, String>() {
 
 			@Override
 			public String execute(ContactMapper mapper) throws Exception {
-				String sender = "";
 				mapper.storeContact(contact);
-				sender = contact.getName();
+				String sender = contact.getName();
 				return sender;
 			}
-			
+
 		}, ContactMapper.class);
 	}
 }
