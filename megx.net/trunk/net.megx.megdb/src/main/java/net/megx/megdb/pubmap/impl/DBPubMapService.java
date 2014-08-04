ba5fbb713fdcd890c386f362a27d90c0f2ae7cba
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-08-04 12:56:58 +0000

    Created Bookmarklet initial version (with test parameters for IWGATE server)

--- megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/pubmap/impl/DBPubMapService.java
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/pubmap/impl/DBPubMapService.java
@@ -1,6 +1,7 @@
 package net.megx.megdb.pubmap.impl;
 
 import net.megx.megdb.BaseMegdbService;
+import net.megx.megdb.exceptions.DBGeneralFailureException;
 import net.megx.megdb.pubmap.PubMapService;
 import net.megx.megdb.pubmap.mappers.PubMapMapper;
 import net.megx.model.pubmap.Article;
@@ -8,19 +9,19 @@ import net.megx.model.pubmap.Article;
 public class DBPubMapService extends BaseMegdbService implements PubMapService {
 
 	@Override
-	public String storeArticles(final Article article) throws Exception {
-			return doInTransaction(new DBTask<PubMapMapper,String>() {
+	public String storeArticle(final Article article)
+			throws DBGeneralFailureException {
+		return doInTransaction(new DBTask<PubMapMapper, String>() {
 
-				@Override
-				public String execute(PubMapMapper mapper) throws Exception {
-					String savedArticle = "";
-					mapper.addArticle(article);
-					savedArticle = article.getMegxBarJSON();
-					return savedArticle;
-				}
-			}, PubMapMapper.class);
-		
+			@Override
+			public String execute(PubMapMapper mapper) throws Exception {
+				String savedArticle = "";
+				mapper.storeArticle(article);
+				savedArticle = article.getMegxBarJSON();
+				return savedArticle;
+			}
+		}, PubMapMapper.class);
+
 	}
-
 
 }
