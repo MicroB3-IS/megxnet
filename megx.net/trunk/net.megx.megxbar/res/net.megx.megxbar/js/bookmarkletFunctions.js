972d035710ffedfb0dfe8c0e14169e306b9f644e
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-08-26 09:31:08 +0000

    Added functionality that allows the user to enter coordinates in degrees, minutes, seconds. Other minor changes. https://colab.mpi-bremen.de/its/browse/MB3_IS-560

--- megx.net/trunk/net.megx.megxbar/res/net.megx.megxbar/js/bookmarkletFunctions.js
+++ megx.net/trunk/net.megx.megxbar/res/net.megx.megxbar/js/bookmarkletFunctions.js
@@ -1,70 +1,182 @@
 $(document).ready(function() {
-	
-	
-		$('#logout').click(function(){
-			log();
-		});
-		
+
+    $('#logout').click(function() {
+        logOut();
+    });
+
+    $('input:radio[name=rb]').click(function(event) {
+
+        if ($('#decimal').is(':checked')) {
+            $('#bookmarkFormDegrees').hide();
+            $('#bookmarkFormDecimal').fadeIn(250);
+        }
+        if ($('#degrees').is(':checked')) {
+            $('#bookmarkFormDecimal').hide();
+            $('#bookmarkFormDegrees').fadeIn(250);
+        }
+    });
+
     $("#submitBookmark").click(function() {
-        $("#bookmarkForm").validate({
-            errorClass: "my-error-class",
-            validClass: "my-valid-class",
-            rules: {
-                longitude: {
-                    required: true,
-                    range: [-180, 180]
+
+        if ($('#bookmarkFormDecimal').is(':visible')) {
+
+            $("#bookmarkFormDecimal").validate({
+                errorClass: "my-error-class",
+                validClass: "my-valid-class",
+                rules: {
+                    latitudeDec: {
+                        required: true,
+                        range: [-90, 90]
+                    },
+                    longitudeDec: {
+                        required: true,
+                        range: [-180, 180]
+                    }
                 },
-                latitude: {
-                    required: true,
-                    range: [-90, 90]
+                messages: {
+                    latitudeDec: "Please enter a valid latitude range.",
+                    longitudeDec: "Please enter a valid longitude range."
                 }
-            },
-            messages: {
-                longitude: "Please enter a valid longitude range.",
-                latitude: "Please enter a valid latitude range."
+            });
+
+            if ($("#bookmarkFormDecimal").valid()) {
+
+                var lon = $('#longitudeDec').val();
+                var lat = $('#latitudeDec').val();
+
+                var data = collectData(lon, lat);
+
+                insertBookmark(data);
             }
-        });
-        
-        if ($("#bookmarkForm").valid()) {
-            insertBookmark();
+        } else {
+
+            var latitudeData = {
+                degrees: $('#latitudeDeg').val(),
+                minutes: $('#latitudeMin').val(),
+                seconds: $('#latitudeSec').val(),
+                from: -90,
+                to: 90,
+                msgElement: "latitudeDegMsg"
+            };
+
+            var longitudeData = {
+                degrees: $('#longitudeDeg').val(),
+                minutes: $('#longitudeMin').val(),
+                seconds: $('#longitudeSec').val(),
+                from: -180,
+                to: 180,
+                msgElement: "longitudeDegMsg"
+            };
+
+            var latitude = dmsToDegrees(latitudeData);
+            var longitude = dmsToDegrees(longitudeData);
+
+            if (latitude != null && longitude != null) {
+                console.log("lat: " + latitude.toFixed(4) + " - lon: " + longitude.toFixed(4));
+
+                var data = collectData(latitude.toFixed(4), longitude.toFixed(4));
+
+                insertBookmark(data);
+            }
         }
     });
 
 });
-function log(){	 
-	 window.open("/megx.net/admin/logout.do",'_blank');
-	 refreshBookmarklet();
-}
-function refreshBookmarklet(){	 
-	document.location.href = document.location.href;
-	   	
+
+function dmsToDegrees(data) {
+
+    var res = null;
+
+    var degreesPatt = /^-?\d+$/g;
+    var minutesPatt = /^\d+$/g;
+    var secondsPatt = /^\d{1,2}(\.\d{1,4})?$/g;
+
+    var degreesRes = degreesPatt.test(data.degrees);
+    var minutesRes = minutesPatt.test(data.minutes);
+    var secondsRes = secondsPatt.test(data.seconds);
+
+    if (degreesRes && (parseInt(data.degrees) >= data.from && parseInt(data.degrees) <= data.to)) {
+
+        var deg = 0;
+        var min = 0;
+        var sec = 0;
+
+        $('#' + data.msgElement).hide();
+        deg = parseInt(data.degrees);
+
+        if ((minutesRes && (parseInt(data.minutes) >= 0 && parseInt(data.minutes) < 60)) || data.minutes == "") {
+
+            $('#' + data.msgElement).hide();
+
+            if (data.minutes == "") {
+                min = 0;
+            } else {
+                min = parseInt(data.minutes);
+            }
+
+            if ((secondsRes && (parseFloat(data.seconds) >= 0 && parseFloat(data.seconds) < 60)) || data.seconds == "") {
+
+                $('#' + data.msgElement).hide();
+
+                if (data.seconds == "") {
+                    sec = 0;
+                } else {
+                    sec = parseFloat(data.seconds);
+                }
+
+                if (deg < 0) {
+                    res = deg - min / 60 - sec / 3600;
+                } else {
+                    res = deg + min / 60 + sec / 3600;
+                }
+
+            } else {
+                $('#' + data.msgElement).text("Seconds value must be 0 or greater and less than 60.").show();
+            }
+        } else {
+            $('#' + data.msgElement).text("Minutes value must be 0 or greater and less than 60.").show();
+        }
+    } else {
+        $('#' + data.msgElement).text("Degrees value must be in range of " + data.from + " to " + data.to + ".").show();
+    }
+
+    return res;
 }
 
+function logOut() {
+    window.open("/megx.net/admin/logout.do", '_blank');
+    refreshBookmarklet();
+}
+
+function refreshBookmarklet() {
+    document.location.href = document.location.href;
+
+}
 
 function emptyMessageDiv() {
     $("#message").html("");
     $("#message").removeAttr("style")
 }
 
-function insertBookmark() {
+function insertBookmark(data) {
 
     $.ajax({
         contentType: 'application/json',
         data: {
-            "article": JSON.stringify(collectData())
+            "article": JSON.stringify(data)
         },
         dataType: 'json',
         success: function(message) {
             $("#message")
-	            .html(
-	                "<button class='close' type='button' onclick='emptyMessageDiv()'>×</button><p>" + message + "</p>");
-	        $("#message").css("background-color", "#DFF0D8");
-	        $("#message").css("border", "1px solid #D6E9C6");
-	        $("#message").css("color", "#468847");
-	        $("#message").css("border-radius", "15px");
-	        $("#message").css("padding-left", "10px");
-	        $("input[type=text]").val("");
-            
+                .html(
+                    "<button class='close' type='button' onclick='emptyMessageDiv()'>×</button><p>" + message + "</p>");
+            $("#message").css("background-color", "#DFF0D8");
+            $("#message").css("border", "1px solid #D6E9C6");
+            $("#message").css("color", "#468847");
+            $("#message").css("border-radius", "15px");
+            $("#message").css("padding-left", "10px");
+            $("input[type=text]").val("");
+
         },
         error: function(a, b, c) {
             $("#message")
@@ -82,21 +194,20 @@ function insertBookmark() {
     });
 }
 
-function collectData() {
+function collectData(lon, lat) {
 
     var megxbar = {
         title: msg.article.title,
         authors: msg.article.authors,
-        lon: $('#longitude').val(),
-        lat: $('#latitude').val()
+        lon: lon,
+        lat: lat
     };
-
 
     var article = {
         pmid: msg.article.pmid,
         url: msg.article.url,
-        lon: $('#longitude').val(),
-        lat: $('#latitude').val(),
+        lon: lon,
+        lat: lat,
         megxBarJSON: JSON.stringify(megxbar),
         articleXML: msg.article.xml
     };
