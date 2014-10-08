b5c37f9c7fa0d8835df5c379b2d0230937362964
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-08 11:49:03 +0000

    Created new Mibig plugin https://colab.mpi-bremen.de/its/browse/MB3_IS-573

---
+++ megx.net/trunk/net.megx.mibig/src/main/java/net/megx/mibig/Activator.java
@@ -0,0 +1,19 @@
+package net.megx.mibig;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    
+
+    @Override
+    protected String getName() {
+        return "net.megx.mibig";
+    }
+
+}
