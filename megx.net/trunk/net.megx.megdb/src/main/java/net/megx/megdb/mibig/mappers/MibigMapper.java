b06efece6d8e7f61f1bef8b2959c5d06272b038a
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-09 14:07:26 +0000

    https://colab.mpi-bremen.de/its/browse/MB3_IS-575

---
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/mibig/mappers/MibigMapper.java
@@ -0,0 +1,9 @@
+package net.megx.megdb.mibig.mappers;
+
+import net.megx.model.mibig.MibigSubmission;
+
+public interface MibigMapper {
+	
+	public void storeMibigSubmission(MibigSubmission mibig);
+
+}
