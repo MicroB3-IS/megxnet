fb752578ed3f395dc5bf4c68789d1eb01eb26fa9
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-23 19:13:58 +0000

    allow more configurations (e.g. wizard), allow adding json to post parameter

--- megx.net/trunk/net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
+++ megx.net/trunk/net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
@@ -1,49 +1,60 @@
-MegxFormWidget = function(cfg) {
-	
-	Alpaca.defaultUI = "jquery-ui";
-    Alpaca.logLevel = Alpaca.ERROR;
-    this.renderForm(cfg);
-    
+MegxFormWidget = function( cfg ) {
+
+  Alpaca.defaultUI = "jquery-ui";
+  Alpaca.logLevel = Alpaca.ERROR;
+  this.renderForm(cfg);
+
 }
 MegxFormWidget.prototype = {
 
-    renderForm : function (cfg) {
-    	
-        var schemaLocation = cfg.schemaLocation;
-        var optionsLocation = cfg.optionsLocation;
-        var inputBundle = {
-          "optionsSource" : optionsLocation,
-          "schemaSource" : schemaLocation,
-          "view" : "VIEW_JQUERYUI_CREATE",
-          "isDynamicCreation" : true,
-          "postRender" : function( field ) {
-            var form = field.form;
+  renderForm : function( cfg ) {
 
-            if ( form ) {
-              form.refreshValidationState(false);
+    var alpacaOptions = {
+      "optionsSource" : cfg.optionsLocation,
+      "schemaSource" : cfg.schemaLocation,
+      "view" : {
 
-              form.registerSubmitHandler(function( e, form ) {
-                  // validate the entire form (top control + all children)
-                  form.validate(true);
-                  // draw the validation state (top control + all children)   
-                  form.refreshValidationState(true);
+        "parent" : "VIEW_JQUERYUI_CREATE",
 
-                  // now display something 
-                  if ( form.isFormValid() ) {
-                    var json = form.getValue();
-                    var formJson = JSON.stringify(field.getValue());
-                   // $('<input/>').attr('name', 'json').attr('value', formJson)
-                   // .appendTo('#contact-form');
-                  } else {
-                    alert("There are problems with the form.  Please make necessary corrections as (hopefully) indicated.");
-                  }
-                  e.stopPropagation();
-                  return true;
-                });
-            }
-          }
-        };
-    	
-      $("#" + cfg.target).alpaca(inputBundle);
-    }   	   
+      },
+
+      "isDynamicCreation" : true,
+
+      "postRender" : function( field ) {
+        var form = field.form;
+
+        if ( form ) {
+          form.refreshValidationState(false);
+
+          form
+            .registerSubmitHandler(function( e, form ) {
+              // validate the entire form (top control + all children)
+              form.validate(true);
+              // draw the validation state (top control + all children)
+              form.refreshValidationState(true);
+ 
+              // now display something
+              if ( form.isFormValid() ) {
+                var json = form.getValue();
+                var formJson = JSON.stringify(field.getValue());
+                alert("formJson: " + formJson);
+                $('<input/>').attr('name', 'json').attr('value', formJson).appendTo("#" + cfg.formId);
+              } else {
+                alert("There is wrong input.  Please make necessary corrections as (hopefully) indicated.");
+                return false;
+              }
+              e.stopPropagation();
+              return true;
+            });
+        }
+      }
+    };
+
+    // now adding wizard to view in case we have one
+    if ( typeof cfg.wizard != "undefined" && cfg.wizard ) {
+      alpacaOptions.view.wizard =  cfg.wizard;
+    }
+
+    $("#" + cfg.target).alpaca(alpacaOptions);
+  }
 }
