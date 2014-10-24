2f1eefc804f46c2a3c6702178c86c363c5513aaa
rkottman <rkottman@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-24 16:53:21 +0000

    added redirect to bgc-submission.html and deleted target directory

--- megx.net/trunk/net.megx.mibig/src/main/java/net/megx/mibig/rest/MibigAPI.java
+++ megx.net/trunk/net.megx.mibig/src/main/java/net/megx/mibig/rest/MibigAPI.java
@@ -1,5 +1,6 @@
 package net.megx.mibig.rest;
 
+import java.net.URI;
 import java.util.Calendar;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
 import net.megx.megdb.exceptions.DBGeneralFailureException;
 import net.megx.megdb.mibig.MibigService;
@@ -19,41 +24,53 @@ import net.megx.ws.core.Result;
 @Path("v1/mibig/v1.0.0")
 public class MibigAPI extends BaseRestService {
 
-	private MibigService service;
+    private MibigService service;
 
-	public MibigAPI(MibigService service) {
-		this.service = service;
-	}
+    @Context
+    private UriInfo uriInfo;
+    
+    public MibigAPI( MibigService service ) {
+        this.service = service;
+    }
 
-	@Path("bgc-registration")
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public String storeMibigSubmission(
-			@FormParam("json") String mibigJson,
-			@Context HttpServletRequest request) {
-		try {
-			if (mibigJson == null) {
-				return toJSON(new Result<String>(true,
-						"json not provided", "bad-request"));
-			}
-			MibigSubmission mibig = new MibigSubmission();
+    @Path("bgc-registration")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response storeMibigSubmission( @FormParam("json") String mibigJson,
+            @Context HttpServletRequest request ) {
 
-			mibig.setRaw(mibigJson);
-			mibig.setSubmitted(Calendar.getInstance().getTime());
-			mibig.setModified(Calendar.getInstance().getTime());
-			// set to version from json string
-			mibig.setVersion(1);
+        try {
+            if (mibigJson == null) {
+                return Response
+                        .status(Status.BAD_REQUEST)
+                        .entity(toJSON(new Result<String>(true,
+                                "json not provided", "bad-request"))).build();
+            }
+            MibigSubmission mibig = new MibigSubmission();
 
-			service.storeMibigSubmission(mibig);
+            mibig.setRaw(mibigJson);
+            mibig.setSubmitted(Calendar.getInstance().getTime());
+            mibig.setModified(Calendar.getInstance().getTime());
+            // set to version from json string
+            mibig.setVersion(1);
 
-			return toJSON("temp-success");
-		} catch (DBGeneralFailureException e) {
-			log.error("Could not store Submission:" + e);
-			return toJSON(handleException(e));
-		} catch (Exception e) {
-			log.error("Server error:" + e);
-			return toJSON(handleException(e));
-		}
-	}
+            service.storeMibigSubmission(mibig);
+            
+            //String address = uriInfo.getAbsolutePathBuilder();
+            URI u = uriInfo.getAbsolutePath();
+            URI location = UriBuilder.fromUri(u).replacePath(request.getContextPath() + "/mibig/bgc-submission" ).build();
+            
+            log.debug("uri absolute=" + u.toASCIIString() );
+            log.debug("context path=" + request.getContextPath() );
+            log.debug("location=" + location.toString());
+            return Response.seeOther(location).build();
+        } catch (DBGeneralFailureException e) {
+            log.error("Could not store Submission:" + e);
+            return Response.serverError().entity( toJSON( "Could not store Submission" ) ).build();
+        } catch (Exception e) {
+            log.error("Server error:" + e);
+            return Response.serverError().entity( toJSON("Server error") ).build();
+        }
+    }
 
 }
