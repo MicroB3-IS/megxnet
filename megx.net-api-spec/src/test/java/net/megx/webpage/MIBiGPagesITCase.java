a273672a6305bc7c3e66fdd8bc44f3ec3cacdce4
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-10-30 15:20:18 +0000

    https://colab.mpi-bremen.de/its/browse/MB3_IS-590

---
+++ megx.net-api-spec/src/test/java/net/megx/webpage/MIBiGPagesITCase.java
@@ -0,0 +1,59 @@
+package net.megx.webpage;
+
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.containsString;
+import net.megx.test.TestServer;
+import net.megx.test.categories.WebPageTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class MIBiGPagesITCase {
+
+	private final String MIBIG_PATH = "/mibig/bgc-registration";
+
+	@Rule
+	public TestServer ts = new TestServer();
+
+	@Test
+	@Category(WebPageTest.class)
+	public void containAlpacaLibrary() {
+		get(MIBIG_PATH)
+				.then()
+				.assertThat()
+				.body(containsString("/net.megx.form-widget/js/alpaca-full-1.1.3.min.js"));
+
+	}
+
+	@Test
+	@Category(WebPageTest.class)
+	public void containAlpacaStyle() {
+		get(MIBIG_PATH)
+				.then()
+				.assertThat()
+				.body(containsString("/net.megx.form-widget/css/alpaca-1.1.3.css"));
+
+	}
+
+	@Test
+	@Category(WebPageTest.class)
+	public void containAlpacaJQuery() {
+		get(MIBIG_PATH)
+				.then()
+				.assertThat()
+				.body(containsString("/net.megx.form-widget/js/jquery.tmpl-1.0.0pre.js"));
+
+	}
+
+	@Test
+	@Category(WebPageTest.class)
+	public void containMegxFormWidget() {
+		get(MIBIG_PATH)
+				.then()
+				.assertThat()
+				.body(containsString("/net.megx.form-widget/js/megx-form-widget-0.1.min.js"));
+
+	}
+
+}
