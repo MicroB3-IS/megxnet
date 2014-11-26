b571ea84c1e07c6546e2fb46be76cb3943ba2bc0
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-11-26 09:27:49 +0000

    MB3_IS-605

--- megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/mibig/impl/DBMibgService.java
+++ megx.net/trunk/net.megx.megdb/src/main/java/net/megx/megdb/mibig/impl/DBMibgService.java
@@ -4,6 +4,7 @@ import net.megx.megdb.BaseMegdbService;
 import net.megx.megdb.exceptions.DBGeneralFailureException;
 import net.megx.megdb.mibig.MibigService;
 import net.megx.megdb.mibig.mappers.MibigMapper;
+import net.megx.model.mibig.BgcDetailSubmission;
 import net.megx.model.mibig.MibigSubmission;
 
 public class DBMibgService extends BaseMegdbService implements MibigService {
@@ -22,4 +23,31 @@ public class DBMibgService extends BaseMegdbService implements MibigService {
 
 	}
 
+	@Override
+	public void storeGeneInfo(final BgcDetailSubmission bgc)
+			throws DBGeneralFailureException {
+		log.debug("Saving  gene info");
+		doInTransaction(new DBTask<MibigMapper, Object>() {
+			@Override
+			public Object execute(MibigMapper mapper) throws Exception {
+				mapper.storeGeneInfo(bgc);
+				return null;
+			}
+		}, MibigMapper.class);
+
+	}
+
+	@Override
+	public void storeNrpsInfo(final BgcDetailSubmission bgc)
+			throws DBGeneralFailureException {
+		log.debug("Saving  nrps info");
+		doInTransaction(new DBTask<MibigMapper, Object>() {
+			@Override
+			public Object execute(MibigMapper mapper) throws Exception {
+				mapper.storeNrpsInfo(bgc);
+				return null;
+			}
+		}, MibigMapper.class);
+
+	}
 }
