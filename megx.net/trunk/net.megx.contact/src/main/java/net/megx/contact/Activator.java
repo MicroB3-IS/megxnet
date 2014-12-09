82b52e39949da1b2140c05926a927dc8854f3a84
jgerken <jgerken@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-12-09 12:38:19 +0000

    Reintegrated branch further-maven-enhancements.
    - completed configuration migration to property files (instead of settings.xml)
    - new implementation of net.megx.mailer
    - changed net.megx.contact to use new mailer implementation

--- megx.net/trunk/net.megx.contact/src/main/java/net/megx/contact/Activator.java
+++ megx.net/trunk/net.megx.contact/src/main/java/net/megx/contact/Activator.java
@@ -1,5 +1,10 @@
 package net.megx.contact;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import net.megx.contact.rest.ContactAPI;
 import net.megx.mailer.BaseMailerService;
 import net.megx.megdb.contact.ContactService;
@@ -10,56 +15,95 @@ import org.apache.commons.logging.LogFactory;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
 import org.chon.web.RegUtils;
-import org.json.JSONObject;
 
 public class Activator extends ResTplConfiguredActivator {
 
     private Log log = LogFactory.getLog(getClass());
 
-    
-    
     @Override
     protected String getName() {
         return "net.megx.contact";
     }
 
+    @Override
+    protected void registerExtensions(JCRApplication app) {
 
+        log.debug("ContactAPI starting up");
+        OSGIUtils.requestService(ContactService.class.getName(),
+                getBundleContext(),
+                new OSGIUtils.OnServiceAvailable<ContactService>() {
 
-	@Override
-	protected void registerExtensions(JCRApplication app) {
-		
-		log.debug("ContactAPI starting up");
-		OSGIUtils.requestService(ContactService.class.getName(),getBundleContext(),
-				new OSGIUtils.OnServiceAvailable<ContactService>() {
+                    @Override
+                    public void serviceAvailable(String name,
+                            final ContactService service) {
+                        log.debug("ContactService received...");
 
-					@Override
-					public void serviceAvailable(String name,
-							final ContactService service) {
-						log.debug("ContactService received...");
-						
-						log.debug("Requesting MailerService service now...");
-						OSGIUtils.requestService(BaseMailerService.class.getName(),getBundleContext(),
-								new OSGIUtils.OnServiceAvailable<BaseMailerService>() {
+                        log.debug("Requesting MailerService service now...");
+                        OSGIUtils.requestService(
+                                BaseMailerService.class.getName(),
+                                getBundleContext(),
+                                new OSGIUtils.OnServiceAvailable<BaseMailerService>() {
 
-									@Override
-									public void serviceAvailable(String name,
-											BaseMailerService mailerService) {
-										log.debug("MailerService service received...");
+                                    @Override
+                                    public void serviceAvailable(String name,
+                                            BaseMailerService mailerService) {
+                                        log.debug("MailerService service received...");
 
+                                        try {
+                                            final Properties configuration = readConfiguration();
+                                            ContactAPI contactAPI = new ContactAPI(
+                                                    service, mailerService,
+                                                    configuration);
+                                            RegUtils.reg(getBundleContext(),
+                                                    ContactAPI.class.getName(),
+                                                    contactAPI, null);
+                                            log.debug("ContactApi started.");
+                                        } catch (final Exception e) {
+                                            log.error(
+                                                    "Error while initializing ContactAPI.",
+                                                    e);
+                                            throw new RuntimeException(
+                                                    "Error while initializing ContactAPI.",
+                                                    e);
+                                        }
+                                    }
 
-										ContactAPI contactAPI = new ContactAPI(service, mailerService, getMailConfig());
-										RegUtils.reg(getBundleContext(), ContactAPI.class.getName(), contactAPI, null);
-										log.debug("ContactApi started.");
-									}
+                                });
+                    }
+                });
+    }
 
-								});
-					}
-				});
-	}
-	
-	private JSONObject getMailConfig(){
-		JSONObject retVal = getConfig().optJSONObject("contactCfg");
-		return retVal;
-	}
+    /**
+     * Reads the configuration from {@code contact.properties} which has to be
+     * present in the classpath.
+     * 
+     * @throws IOException
+     *             if an IO error occurs.
+     * @throws FileNotFoundException
+     *             if {@code mailer.properties} cannot be found in the
+     *             classpath.
+     */
+    private final Properties readConfiguration() throws IOException {
+
+        final Properties properties = new Properties();
+        InputStream inputStream = null;
+
+        try {
+            inputStream = this.getClass().getClassLoader()
+                    .getResourceAsStream("contact.properties");
+
+            if (inputStream == null) {
+                throw new FileNotFoundException(
+                        "property file contact.properties not found in the classpath");
+            }
+
+            properties.load(inputStream);
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return properties;
+    }
 
 }
