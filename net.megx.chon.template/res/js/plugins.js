26b9c63809f345a575aa42800866e2c75fd2d96f
jgerken <jgerken@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-12-17 15:58:52 +0000

    initial bootstrap template, menu is not yet responsive and misses a lot of the elements

---
+++ net.megx.chon.template/res/js/plugins.js
@@ -0,0 +1,34 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+//// Place any jQuery/helper plugins in here.
+//var $window = jQuery(window);
+//var header = jQuery('#main-menu');
+//$window.scroll(function(){
+//    if ($window.scrollTop() >= 148) {
+//       header.addClass('fixed-header');
+//    }
+//    else {
+//       header.removeClass('fixed-header');
+//    }
+//});
