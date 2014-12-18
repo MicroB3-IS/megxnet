c9f20982f59ae8a8c56aa6ee2e70e6b7e2414175
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2014-12-18 15:26:55 +0000

    MB3_IS-613 MB3_IS-614 #comment mailer implemented in net.megx.security.filter, activation mail for registering new user now active

--- net.megx.contact/src/main/java/net/megx/contact/rest/ContactAPI.java
+++ net.megx.contact/src/main/java/net/megx/contact/rest/ContactAPI.java
@@ -28,102 +28,104 @@ import net.megx.ws.core.BaseRestService;
 @Path("v1/contact/v1.0.0")
 public class ContactAPI extends BaseRestService {
 
-    private static final String PROPERTY_KEY_SENDER_MAIL = "net.megx.contact.mail.sender";
-    private static final String PROPERTY_KEY_RECIPIENT_MAILS = "net.megx.contact.mail.recipients";
+  private static final String PROPERTY_KEY_SENDER_MAIL = "net.megx.contact.mail.sender";
+  private static final String PROPERTY_KEY_RECIPIENT_MAILS = "net.megx.contact.mail.recipients";
 
-    private ContactService service;
-    private BaseMailerService mailerService;
-    private final String contactEmailSender;
-    private final String contactEmailRecipients;
+  private ContactService service;
+  private BaseMailerService mailerService;
+  private final String contactEmailSender;
+  private final String contactEmailRecipients;
 
-    public ContactAPI(ContactService service, BaseMailerService mailerService, final Properties configuration) {
+  public ContactAPI(ContactService service, BaseMailerService mailerService,
+      final Properties configuration) {
 
-        if (service == null) {
-            throw new IllegalArgumentException("service must not be null.");
-        }
-        if (mailerService == null) {
-            throw new IllegalArgumentException("mailerService must be null.");
-        }
-        
-        this.verifyConfiguration(configuration);
-        
-        this.service = service;
-        this.mailerService = mailerService;
-        this.contactEmailSender = configuration.getProperty(PROPERTY_KEY_SENDER_MAIL);
-        this.contactEmailRecipients = configuration.getProperty(PROPERTY_KEY_RECIPIENT_MAILS);
+    if (service == null) {
+      throw new IllegalArgumentException("service must not be null.");
+    }
+    if (mailerService == null) {
+      throw new IllegalArgumentException("mailerService must not be null.");
     }
 
-    @Path("store-contact")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response storeContactMail(@FormParam("email") String email,
-            @FormParam("name") String name,
-            @FormParam("comment") String comment,
-            @Context HttpServletRequest request) {
+    this.verifyConfiguration(configuration);
 
-        Date date = Calendar.getInstance().getTime();
-        Contact contact = new Contact();
-        contact.setEmail(email);
-        contact.setName(name);
-        contact.setCreated(date);
-        contact.setComment(comment);
-        String url = "";
-        URI uri = null;
+    this.service = service;
+    this.mailerService = mailerService;
+    this.contactEmailSender = configuration
+        .getProperty(PROPERTY_KEY_SENDER_MAIL);
+    this.contactEmailRecipients = configuration
+        .getProperty(PROPERTY_KEY_RECIPIENT_MAILS);
+  }
 
-        try {
-            sendMail(email, name, comment);
-            service.storeContact(contact);
-            url = request.getScheme() + "://" + request.getServerName() + ":"
-                    + request.getServerPort() + request.getContextPath();
-            uri = new URI(url);
-        } catch (DBGeneralFailureException e) {
-            log.error("Could not save mail", e);
-            throw new WebApplicationException(e,
-                    Response.Status.INTERNAL_SERVER_ERROR);
-        } catch (URISyntaxException e) {
-            log.error("Wrong URI" + url, e);
-        } catch (Exception e) {
-            log.error("Error occured", e);
-            throw new WebApplicationException(e,
-                    Response.Status.INTERNAL_SERVER_ERROR);
-        }
+  @Path("store-contact")
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response storeContactMail(@FormParam("email") String email,
+      @FormParam("name") String name, @FormParam("comment") String comment,
+      @Context HttpServletRequest request) {
 
-        return Response.seeOther(uri).build();
+    Date date = Calendar.getInstance().getTime();
+    Contact contact = new Contact();
+    contact.setEmail(email);
+    contact.setName(name);
+    contact.setCreated(date);
+    contact.setComment(comment);
+    String url = "";
+    URI uri = null;
+
+    try {
+      sendMail(email, name, comment);
+      service.storeContact(contact);
+      url = request.getScheme() + "://" + request.getServerName() + ":"
+          + request.getServerPort() + request.getContextPath();
+      uri = new URI(url);
+    } catch (DBGeneralFailureException e) {
+      log.error("Could not save mail", e);
+      throw new WebApplicationException(e,
+          Response.Status.INTERNAL_SERVER_ERROR);
+    } catch (URISyntaxException e) {
+      log.error("Wrong URI" + url, e);
+    } catch (Exception e) {
+      log.error("Error occured", e);
+      throw new WebApplicationException(e,
+          Response.Status.INTERNAL_SERVER_ERROR);
     }
 
-    private void sendMail(String email, String name, String comment)
-            throws AddressException, MessagingException {
-        final MailMessage message = new MailMessage(name
-                + " (megx.net contact form) <" + this.contactEmailSender + ">");
-        message.setReplyToAddress(email);
-        message.addRecipients(this.contactEmailRecipients);
-        message.setSubject("User feedback");
-        message.setMessageBody(comment);
-        mailerService.send(message);
+    return Response.seeOther(uri).build();
+  }
+
+  private void sendMail(String email, String name, String comment)
+      throws AddressException, MessagingException {
+    final MailMessage message = new MailMessage(name
+        + " (megx.net contact form) <" + this.contactEmailSender + ">");
+    message.setReplyToAddress(email);
+    message.addRecipients(this.contactEmailRecipients);
+    message.setSubject("User feedback");
+    message.setMessageBody(comment);
+    mailerService.send(message);
+  }
+
+  private void verifyConfiguration(final Properties configuration) {
+
+    final StringBuilder errorMessage = new StringBuilder(
+        "ContactAPI configuration is incomplete.");
+    boolean error = false;
+
+    if (configuration.getProperty(PROPERTY_KEY_SENDER_MAIL) == null
+        || configuration.getProperty(PROPERTY_KEY_SENDER_MAIL).trim().isEmpty()) {
+      errorMessage.append(" Missing configuration for sender mail address.");
+      error = true;
+    }
+    if (configuration.getProperty(PROPERTY_KEY_RECIPIENT_MAILS) == null
+        || configuration.getProperty(PROPERTY_KEY_RECIPIENT_MAILS).trim()
+            .isEmpty()) {
+      errorMessage
+          .append(" Missing configuration for recipient(s) mail address(es).");
+      error = true;
     }
 
-    private void verifyConfiguration(final Properties configuration) {
-
-        final StringBuilder errorMessage = new StringBuilder(
-                "ContactAPI configuration is incomplete.");
-        boolean error = false;
-        
-        if (configuration.getProperty(PROPERTY_KEY_SENDER_MAIL) == null
-                || configuration.getProperty(PROPERTY_KEY_SENDER_MAIL).trim()
-                        .isEmpty()) {
-            errorMessage.append(" Missing configuration for sender mail address.");
-            error = true;
-        }
-        if (configuration.getProperty(PROPERTY_KEY_RECIPIENT_MAILS) == null
-                || configuration.getProperty(PROPERTY_KEY_RECIPIENT_MAILS).trim()
-                        .isEmpty()) {
-            errorMessage.append(" Missing configuration for recipient(s) mail address(es).");
-            error = true;
-        }
-
-        if (error) {
-            throw new IllegalArgumentException(errorMessage.toString());
-        }
+    if (error) {
+      throw new IllegalArgumentException(errorMessage.toString());
     }
+  }
 
 }
