82ed86620c068ceb679f03b47f2c269511fc8dfe
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2015-01-13 14:35:31 +0000

    New megx.net.datatable-widget structure.

---
+++ net.megx.datatable-widget/src/main/java/net/megx/datatable/widget/Activator.java
@@ -0,0 +1,27 @@
+package net.megx.datatable.widget;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    
+
+    @Override
+    protected String getName() {
+        return "net.megx.datatable-widget";
+    }
+
+
+
+    @Override
+    protected void registerExtensions(JCRApplication arg0) {
+      // TODO Auto-generated method stub
+      
+    }
+
+}
