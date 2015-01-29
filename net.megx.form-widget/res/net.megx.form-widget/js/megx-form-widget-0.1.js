cc3dd6d8a905c10cc66214aed87749316d76f791
jgerken <jgerken@82e9701a-15f3-4962-89cc-9b74a964515e>
2015-01-29 15:59:03 +0000

    merged next version of megx-form-widget

--- net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
+++ net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
@@ -1,117 +1,121 @@
 MegxFormWidget = function(cfg) {
-
   Alpaca.logLevel = Alpaca.ERROR;
+  this.cfg = cfg;
   this.renderForm(cfg);
 
 }
-MegxFormWidget.prototype = {
 
-  renderForm : function(cfg) {
+MegxFormWidget.prototype.submitForm = function(field) {
+  
+  var self = this;
+  var form = field.form;
+  var url = form.attributes.action;
+  var ajaxMethod = form.attributes.method;
 
-    var alpacaOptions = {
-      "optionsSource" : cfg.optionsLocation,
-      "schemaSource" : cfg.schemaLocation,
-      "view" : {
-        "parent" : cfg.parentView,
-        "displayReadonly" : true,
-      },
-      "isDynamicCreation" : true
-    };
+  if (form) {
 
-    var postRender = function(field) {
+    form.registerSubmitHandler(function(e, form) {
+      return false;
+    });
+    var id = "#" + form.id;
+    jQuery('button[type="submit"]', id).click(function() {
 
-      var form = field.form;
-      var url = form.attributes.action;
-      var ajaxMethod = form.attributes.method;
+      if (field.isValid(true)) {
 
-      if (form) {
+        var data = field.getValue();
+        data.json = JSON.stringify(field.getValue());
+        $.ajax({
+          type : ajaxMethod,
+          url : url,
+          data : data
+        }).success(function(data, textStatus, jqXHR) {
 
-        form.registerSubmitHandler(function(e, form) {
-          return false;
-        });
-        var id = "#" + form.id;
-        jQuery('button[type="submit"]', id).click(function() {
-          if (field.isValid(true)) {
+          if (data.redirectUrl) {
+            alert("Your submission was successfull. Thanks!");
+            document.location.href = data.redirectUrl;
+          } else {
 
-            var data = field.getValue();
-            data.json = JSON.stringify(field.getValue());
-            jQuery.ajax({
-              type : ajaxMethod,
-              url : url,
-              data : data
-            }).done(function(data, textStatus, jqXHR) {
+            if (data.message) {
+              alert(data.message);
+              document.location.href = ctx.siteUrl;
+            } else {
+              alert("Your submission was successfull. Thanks!");
+              document.location.href = ctx.siteUrl;
+            }
+          }
+        }).error(function(data, textStatus, jqXHR) {
 
-              if (data.redirectUrl) {
-                alert("Your submission was successfull. Thanks!");
-                document.location.href = data.redirectUrl;
-              } else {
+          var errorElement = jQuery('#' + MegxFormWidget.cfg.errorTarget);
+          if (data.responseJSON) {
 
-                if (data.message) {
-                  alert(data.message);
-                  document.location.href = ctx.siteUrl;
-                } else {
-                  alert("Your submission was successfull. Thanks!");
-                  document.location.href = ctx.siteUrl;
-                }
-              }
-            }).fail(function(data, textStatus, jqXHR) {
+            var responseMsg = data.responseJSON;
 
-              var errorElement = jQuery('#' + cfg.errorTarget);
-              if (data.responseJSON && data.responseJSON != null) {
+            if (responseMsg.redirectUrl) {
+              document.location.href = responseMsg.redirectUrl;
+            } else {
 
-                var responseMsg = data.responseJSON;
-
-                if (responseMsg.redirectUrl) {
-                  document.location.href = responseMsg.redirectUrl;
-                } else {
-
-                  if (responseMsg.message) {
-                    errorElement.text(responseMsg.message).show();
-                  } else {
-                    errorElement.text("Server error, please try again later.").show();
-                  }
-                }
-
+              if (responseMsg.message && responseMsg.message != "") {
+                errorElement.text(responseMsg.message).show();
               } else {
                 errorElement.text("Server error, please try again later.").show();
               }
-            });
+            }
+          } else {
+            errorElement.text("Server error, please try again later.").show();
           }
         });
       }
-    };
-
-    var buts = {
-      "done" : {
-        "validateOnClick" : true,
-        "onClick" : function() {
-          submitForm(jQuery("#" + cfg.formId))
-        }
-      },
-
-      "next" : {
-        "validateOnClick" : true,
-        "onClick" : function() {
-        }
-      },
-      "prev" : {
-        "validateOnClick" : true,
-        "onClick" : function() {
-        }
-      }
-    };
-    // now adding wizard to view in case we have one
-    if (typeof cfg.wizard != "undefined" && cfg.wizard) {
-      alpacaOptions.view.wizard = cfg.wizard;
-      alpacaOptions.view.wizard.buttons = buts;
-    }
-
-    if (typeof cfg.postRender != "undefined" && cfg.postRender) {
-      alpacaOptions.postRender = cfg.postRender;
-    } else {
-      alpacaOptions.postRender = postRender;
-    }
-
-    jQuery("#" + cfg.target).alpaca(alpacaOptions);
+    });
   }
+}
+
+MegxFormWidget.prototype.renderForm = function(cfg) {
+  var self = this;
+
+  var alpacaOptions = {
+    "optionsSource" : cfg.optionsLocation,
+    "schemaSource" : cfg.schemaLocation,
+    "view" : {
+      "parent" : cfg.parentView,
+      "displayReadonly" : true,
+    },
+    "isDynamicCreation" : true
+  };
+
+  var buts = {
+    "done" : {
+      "validateOnClick" : true,
+      "onClick" : function() {
+        // alert("done clicked.");
+        self.submitForm($("#" + cfg.formId))
+      }
+    },
+
+    "next" : {
+      "validateOnClick" : true,
+      "onClick" : function() {
+        // alert("Fired next.onClick handler");
+      }
+    },
+    "prev" : {
+      "validateOnClick" : true,
+      "onClick" : function() {
+        // alert("Fired prev.onClick handler");
+      }
+    }
+  };
+  // now adding wizard to view in case we have one
+  if (typeof cfg.wizard != "undefined" && cfg.wizard) {
+    alpacaOptions.view.wizard = cfg.wizard;
+    alpacaOptions.view.wizard.buttons = buts;
+  }
+
+  if (typeof cfg.postRender != "undefined" && cfg.postRender) {
+    alpacaOptions.postRender = cfg.postRender;
+  } else {
+    alpacaOptions.postRender = self.submitForm;
+  }
+
+  $("#" + cfg.target).alpaca(alpacaOptions);
+
 }
