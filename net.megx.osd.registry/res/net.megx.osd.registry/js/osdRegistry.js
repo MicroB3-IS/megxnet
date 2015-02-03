1fd038e066a608a5c839b5324bc67814dfe6784e
jgerken <jgerken@82e9701a-15f3-4962-89cc-9b74a964515e>
2015-02-03 18:57:23 +0000

    Particial conversion of OSD site registry: list, view, add, delete

--- net.megx.osd.registry/res/net.megx.osd.registry/js/osdRegistry.js
+++ net.megx.osd.registry/res/net.megx.osd.registry/js/osdRegistry.js
@@ -1,225 +1,212 @@
-$(document).ready(function() {
-	
-	$("#submitOSDParticipant").click(function() {
-		$("#participantForm").validate({
-			errorPlacement: function(error, element) {
-				error.appendTo(element.parent("td").next("td"));
-			},
-			rules: {
-				osdID: {
-					required: true
-				},
-				siteName: {
-					required: true
-				},
-				institution: {
-	 				required: true
-				},
-				institutionAddress: {
-					required: true
-				},
-				country: {
-					required: true
-				},
-				institutionWebAddress: {
-					required: false,
-					url: true
-				},
-				siteCoordinator: {
-					required: true
-				},
-				coordinatorEmail: {
-					required: false,
-					email: true
-				},
-				siteLat: {
-					required: true,
-					number: true
-				},
-				siteLong: {
-					required: true,
-					number: true
-				},
-				institutionLat: {
-					required: true,
-					number: true
-				},
-				institutionLong: {
-					required: true,
-					number: true
-				}
-			}
-		});
-		if($("#participantForm").valid()){
-			submitParticipant();
-		}
-	});
-	
-	var ajaxCall = function(httpVerb, url, data, successHandler, errorHandler){
-		$.ajax({
-			  type: httpVerb,
-			  url: url,
-			  data: data,
-			  contentType : "application/json",
-			  success: function(data){
-				  successHandler.call(this, data);
-			  },
-			  error: function(jqXHR, textStatus, errorThrown ){
-				  errorHandler.call(this, data, jqXHR, textStatus, errorThrown );
-			  }
-		});
-	};
-	
-	var loadParticipantData = function(id, handler){
-		var url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/getParticipant";
-		
-		ajaxCall("GET", url, {id:id}, handler);
-	};
+jQuery(document).ready(function() {
 
-	var submitParticipant = function() {
-		
-		var url = "";
-		var redirectUrl = ctx.siteUrl + "/osd-registry/list";
-		
-		if(isInEditMode){
-			url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/updateParticipant";
-		} else{
-			url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/addParticipant";
-		}
-		
-		ajaxCall(
-				"POST", 
-				url, 
-				{"participant":JSON.stringify(retrieveFormData())},
-				function(){
-					window.location.href = redirectUrl;
-				}
-			);
-	};
-	
-	var retrieveFormData = function(){
-		var populatedObject = {
-			"osdID" : $("#osdID").val(),
-			"siteName" : $("#siteName").val(),
-			"institution" : $("#institution").val(),
-			"institutionAddress" : $("#institutionAddress").val(),
-			"country" :$("#country").val(),
-			"institutionWebAddress" : $("#institutionWebAddress").val(),
-			"siteCoordinator" : $("#siteCoordinator").val(),
-			"coordinatorEmail" : $("#coordinatorEmail").val(),
-			"siteLat" : $("#siteLat").val(),
-			"siteLong" : $("#siteLong").val(),
-			"institutionLat" : $("#institutionLat").val(),
-			"institutionLong" : $("#institutionLong").val(),
-			"id" : isInEditMode == true ? participantID : ""
-		};
-		
-		return populatedObject;
-	};
-	
-	var populateFormData = function(retrievedData){
-		$("#osdID").val(retrievedData.osdID);
-		$("#siteName").val(retrievedData.siteName);
-		$("#institution").val(retrievedData.institution);
-		$("#institutionAddress").val(retrievedData.institutionAddress);
-		$("#country").val(retrievedData.country);
-		$("#institutionWebAddress").val(retrievedData.institutionWebAddress);
-		$("#siteCoordinator").val(retrievedData.siteCoordinator);
-		$("#coordinatorEmail").val(retrievedData.coordinatorEmail);
-		$("#siteLat").val(retrievedData.siteLat);
-		$("#siteLong").val(retrievedData.siteLong);
-		$("#institutionLat").val(retrievedData.institutionLat);
-		$("#institutionLong").val(retrievedData.institutionLong);
-	};
-	
-	if(typeof isInEditMode != 'undefined' && isInEditMode){
-		loadParticipantData(participantID, populateFormData);
-	}
-	
-	$('tbody').on('click', 'a.deleteParticipantClass', function(){
-		var recordID = $(this).attr("id");
-		$("#deleteDialog").data("id", recordID).dialog('open');
-		return false;
-	});
-	
-	var deleteParticipant = function(id){
-		var url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/deleteParticipant";
-		var redirectUrl = ctx.siteUrl + "/osd-registry/list";
-		
-		ajaxCall(
-				"POST",
-				url, 
-				{id:id}, 
-				function(){
-					$("#deleteDialog").dialog( "close" );
-					window.location.href = redirectUrl;
-				},
-				function(){
-					$("#deleteDialog").dialog( "close" );
-			});
-	};
-	
-	$("#deleteDialog").dialog({
-		  resizable: false,
-	      autoOpen: false,
-		  height: 160,
-		  width: 320,
-	      modal: true,
-	      title: "Confirm delete",
-	      buttons: {
-	        Ok: function() {
-	          var id = $(this).data("id");
-	          deleteParticipant(id);
-	        },
-	        Cancel: function() {
-	          $( this ).dialog( "close" );
-	        }
-	      }
-	});
-	
-	$('tbody').on('click', 'a.viewParticipantClass', function(){
-		var recordID = $(this).attr("name");
-		$('.MapBox div').each(function(){
-			if($(this).css("z-index")){
-				var z = $(this).css("z-index") - 100;
-				if(!$(this).attr("z")){
-					$(this).css("z-index", z);
-					$(this).attr("z", "true");
-				}
-				
-			}
-		});
-		$("#viewDialog").data("id", recordID).dialog("open");
-		return false;
-	});
-	
-	$("#viewDialog").dialog({
-		  resizable: false,
-	      autoOpen: false,
-		  height: 'auto',
-		  width: 'auto',
-	      modal: true,
-	      title: "OSD participant info",
-	      buttons: {
-	        Close: function() {
-	          $( this ).dialog( "close" );
-	        }
-	      },
-	      open: function(){
-	    	  var id = $(this).data("id");
-	    	  loadParticipantData(id, populateViewDialog);
-	      }
-	});
-	
-	var populateViewDialog = function(retrievedData){
-		$("#siteLatVal").text(retrievedData.siteLat);
-		$("#siteLongVal").text(retrievedData.siteLong);
-		$("#institutionLatVal").text(retrievedData.institutionLat);
-		$("#institutionLongVal").text(retrievedData.institutionLong);
-	};
-	
-	$(".addNewParticipant").click(function(){
-		window.location.href = ctx.siteUrl + "/osd-registry/add";
-	});
+  jQuery("#submitOSDParticipant").click(function() {
+    jQuery("#participantForm").validate({
+      highlight : function(element, errorClass, validClass) {
+        jQuery(element).parent().parent().addClass(errorClass).removeClass(validClass);
+      },
+      unhighlight : function(element, errorClass, validClass) {
+        jQuery(element).parent().parent().removeClass(errorClass).addClass(validClass);
+      },
+      errorClass : 'has-error',
+      validClass : 'has-success',
+      errorPlacement : function(error, element) {
+        error.appendTo(element.parent().parent().children(".help-block"));
+      },
+      rules : {
+        osdID : {
+          required : true
+        },
+        siteName : {
+          required : true
+        },
+        institution : {
+          required : true
+        },
+        institutionAddress : {
+          required : true
+        },
+        country : {
+          required : true
+        },
+        institutionWebAddress : {
+          required : false,
+          url : true
+        },
+        siteCoordinator : {
+          required : true
+        },
+        coordinatorEmail : {
+          required : false,
+          email : true
+        },
+        siteLat : {
+          required : true,
+          number : true
+        },
+        siteLong : {
+          required : true,
+          number : true
+        },
+        institutionLat : {
+          required : true,
+          number : true
+        },
+        institutionLong : {
+          required : true,
+          number : true
+        }
+      }
+    });
+    if (jQuery("#participantForm").valid()) {
+      submitParticipant();
+    }
+  });
+
+  var ajaxCall = function(httpVerb, url, data, successHandler, errorHandler) {
+    jQuery.ajax({
+      type : httpVerb,
+      url : url,
+      data : data,
+      contentType : "application/json",
+      success : function(data) {
+        successHandler.call(this, data);
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+        var message = 'Your request could not be completed.';
+        if (jqXHR.status == 500) {
+          var data = JSON.parse(jqXHR.responseText).data;
+          var cause = data.cause;
+          while (cause && cause.cause) {
+            cause = cause.cause;
+          }
+          if (cause && cause.message) {
+            message += " " + cause.message;
+          }
+        }
+        toastr.error(message, errorThrown)
+        if (errorHandler) {
+          errorHandler.call(this, data, jqXHR, textStatus, errorThrown);
+        }
+      }
+    });
+  };
+
+  var loadParticipantData = function(id, handler) {
+    var url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/getParticipant";
+
+    ajaxCall("GET", url, {
+      id : id
+    }, handler);
+  };
+
+  var submitParticipant = function() {
+
+    var url = "";
+    var redirectUrl = ctx.siteUrl + "/osd-registry/list";
+
+    if (isInEditMode) {
+      url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/updateParticipant";
+    } else {
+      url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/addParticipant";
+    }
+
+    ajaxCall("POST", url, {
+      "participant" : JSON.stringify(retrieveFormData())
+    }, function() {
+      window.location.href = redirectUrl;
+    });
+  };
+
+  var retrieveFormData = function() {
+    var populatedObject = {
+      "osdID" : jQuery("#osdID").val(),
+      "siteName" : jQuery("#siteName").val(),
+      "institution" : jQuery("#institution").val(),
+      "institutionAddress" : jQuery("#institutionAddress").val(),
+      "country" : jQuery("#country").val(),
+      "institutionWebAddress" : jQuery("#institutionWebAddress").val(),
+      "siteCoordinator" : jQuery("#siteCoordinator").val(),
+      "coordinatorEmail" : jQuery("#coordinatorEmail").val(),
+      "siteLat" : jQuery("#siteLat").val(),
+      "siteLong" : jQuery("#siteLong").val(),
+      "institutionLat" : jQuery("#institutionLat").val(),
+      "institutionLong" : jQuery("#institutionLong").val(),
+      "id" : isInEditMode == true ? participantID : ""
+    };
+
+    return populatedObject;
+  };
+
+  var populateFormData = function(retrievedData) {
+    jQuery("#osdID").val(retrievedData.osdID);
+    jQuery("#siteName").val(retrievedData.siteName);
+    jQuery("#institution").val(retrievedData.institution);
+    jQuery("#institutionAddress").val(retrievedData.institutionAddress);
+    jQuery("#country").val(retrievedData.country);
+    jQuery("#institutionWebAddress").val(retrievedData.institutionWebAddress);
+    jQuery("#siteCoordinator").val(retrievedData.siteCoordinator);
+    jQuery("#coordinatorEmail").val(retrievedData.coordinatorEmail);
+    jQuery("#siteLat").val(retrievedData.siteLat);
+    jQuery("#siteLong").val(retrievedData.siteLong);
+    jQuery("#institutionLat").val(retrievedData.institutionLat);
+    jQuery("#institutionLong").val(retrievedData.institutionLong);
+  };
+
+  if (typeof isInEditMode != 'undefined' && isInEditMode) {
+    loadParticipantData(participantID, populateFormData);
+  }
+
+  var deleteParticipant = function(id) {
+    console.log("deleting " + id);
+    var url = ctx.siteUrl + "/ws/v1/OSDRegistry/v1.0.0/deleteParticipant";
+    var redirectUrl = ctx.siteUrl + "/osd-registry/list";
+
+    ajaxCall("POST", url, {
+      id : id
+    }, function() {
+      jQuery('#deleteDialog').modal('hide');
+      window.location.href = redirectUrl;
+    }, function() {
+      jQuery('#deleteDialog').modal('hide');
+    });
+  };
+
+  jQuery('#deleteDialog').on('shown.bs.modal', function() {
+    jQuery('#deleteDialog-cancelButton').focus();
+  });
+
+  jQuery('#deleteDialog').on('show.bs.modal', function(event) {
+    var button = jQuery(event.relatedTarget) // Button that triggered the modal
+    var id = button.data('delete-id') // Extract info from data-* attributes
+    var modal = jQuery(this)
+    modal.find('.modal-body').find('.delete-id').text(id);
+    modal.find('#deleteDialog-deleteButton').off().on('click', function(event) {
+      deleteParticipant(id)
+    });
+  });
+  
+  jQuery('#viewDialog').on('shown.bs.modal', function() {
+    jQuery('#viewDialog').find('button').focus();
+  });
+
+  jQuery('#viewDialog').on('show.bs.modal', function(event) {
+    var button = jQuery(event.relatedTarget) // Button that triggered the modal
+    var id = button.data('view-id') // Extract info from data-* attributes
+    loadParticipantData(id, populateViewDialog);
+  });
+
+  var populateViewDialog = function(retrievedData) {
+    jQuery("#viewDialog-osdID").text(retrievedData.osdID);
+    jQuery("#viewDialog-siteName").text(retrievedData.siteName);
+    jQuery("#viewDialog-siteLatVal").text(retrievedData.siteLat);
+    jQuery("#viewDialog-siteLongVal").text(retrievedData.siteLong);
+    jQuery("#viewDialog-institutionLatVal").text(retrievedData.institutionLat);
+    jQuery("#viewDialog-institutionLongVal").text(retrievedData.institutionLong);
+  };
+
+  jQuery(".addNewParticipant").click(function() {
+    window.location.href = ctx.siteUrl + "/osd-registry/add";
+  });
 
 });
