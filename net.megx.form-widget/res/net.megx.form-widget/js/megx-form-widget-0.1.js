2ccb3713ce3f6c663b728973903fc5e1501528fe
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2015-02-10 14:57:29 +0000

    MB3_IS-627 #comment now using toastr java script library for info/error messages, merged form-widget with bootstrap's version

--- net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
+++ net.megx.form-widget/res/net.megx.form-widget/js/megx-form-widget-0.1.js
@@ -1,140 +1,131 @@
 MegxFormWidget = function(cfg) {
-	var self = this;
-    Alpaca.defaultUI = "jquery-ui";
+    jQuery(
+            "<div id='" + cfg.target + "-loading-animation'><span class='glyphicon glyphicon-refresh glyphicon-refresh-animate'></span> Loading form</div>")
+        .insertBefore("#" + cfg.target);
     Alpaca.logLevel = Alpaca.ERROR;
-    self.cfg = cfg;
-    self.renderForm(cfg);
+    Alpaca.defaultUI = "jquery-ui";
+    this.cfg = cfg;
+    this.renderForm(cfg);
 
 }
 
-MegxFormWidget.prototype.submitForm = function(field){
-	var self = this;
-	var form = field.form;
+MegxFormWidget.prototype.submitForm = function(field) {
+
+    var self = this;
+    var form = field.form;
     var url = form.attributes.action;
     var ajaxMethod = form.attributes.method;
-    
-    var errorElement = jQuery('#' + MegxFormWidget.cfg.errorTarget);
 
     if (form) {
-
-        form.registerSubmitHandler(function(e, form) {
-            return false;
-        });
-        
-        jQuery(".alpaca-fieldset").keyup(function() {
+    	
+    	jQuery(".alpaca-fieldset").keyup(function() {
             if (field.isValid(true)) {
             	form.enableSubmitButton();
             }
         });
 
-        form.buttons.submit.click(function() {
+        form.registerSubmitHandler(function(e, form) {
+            return false;
+        });
+        var id = "#" + form.id;
+
+        var submitButton;
+        if (MegxFormWidget.cfg.wizard) {
+            submitButton = jQuery('.alpaca-wizard-button-done', id);
+        } else {
+            submitButton = jQuery('button[type="submit"]', id);
+        }
+        // hack to change the CSS classes of the submit button
+        // might be possible to do this using Alpaca templates
+        // but could not figure out how
+        submitButton.removeClass("btn-default");
+        submitButton.addClass("btn-primary");
+
+        submitButton.click(function() {
         	
         	form.disableSubmitButton();
 
-        	if (field.isValid(true)) {
+            if (field.isValid(true)) {
 
                 var data = field.getValue();
                 data.json = JSON.stringify(field.getValue());
                 jQuery.ajax({
                     type: ajaxMethod,
                     url: url,
-                    data: data,
-                    success: function(data, textStatus, jqXHR) {
-                    	
-                    	if(data.redirectUrl && data.redirectUrl != ""){
-                    		alert("Your submission was successfull. Thanks!");
+                    data: data
+                }).success(function(data, textStatus, jqXHR) {
+
+                    if (data.redirectUrl) {
+                    	toastr.info("Your submission was successfull. Thanks!");
+                    	setTimeout(function() {
                     		document.location.href = data.redirectUrl;
-                    	} else {
-                    		
-                    		if(data.message && data.message != ""){
-                    			alert(data.message);
+                		}, 3000);
+                    } else {
+
+                        if (data.message) {
+                        	toastr.info(data.message);
+                            setTimeout(function() {
+                            	document.location.href = ctx.siteUrl;
+                    		}, 3000);
+                        } else {
+                        	toastr.info("Your submission was successfull. Thanks!");
+                            setTimeout(function() {
                     			document.location.href = ctx.siteUrl;
-                    		} else{
-                    			alert("Your submission was successfull. Thanks!");
-                        		document.location.href = ctx.siteUrl;
-                    		}
-                    	}
-                    },
-                    error: function(data, textStatus, jqXHR) {
-                    	
-                    	form.enableSubmitButton();
-                    	
-                    	if(data.responseJSON && data.responseJSON != ""){
-                    		
-                    		var responseMsg = data.responseJSON;
-                        	
-                        	if(responseMsg.redirectUrl && responseMsg.redirectUrl != ""){
-                        		document.location.href = responseMsg.redirectUrl;
-                        	} else {
-                        		
-                        		if(responseMsg.message && responseMsg.message != ""){
-                        			errorElement.text(responseMsg.message).show();
-                        		} else{
-                        			errorElement.text("Server error, please try again later.").show();
-                        		}
-                        	}
-                    		
-                    	} else {
-                    		errorElement.text("Server error, please try again later.").show();
-                    	}
-                    	
+                    		}, 3000);
+                        }
                     }
+                }).error(function(data, textStatus, jqXHR) {
+                    if (data.responseJSON) {
+
+                        var responseMsg = data.responseJSON;
+
+                        if (responseMsg.redirectUrl) {
+                            document.location.href = responseMsg.redirectUrl;
+                        } else {
+
+                            if (responseMsg.message) {
+                                toastr.error(responseMsg.message, jqXHR);
+                            } else {
+                                toastr.error("Server error, please try again later.", jqXHR);
+                            }
+                        }
+                    } else {
+                        toastr.error("Server error, please try again later.", jqXHR);
+                    }
+                }).always(function(data, textStatus, jqXHR) {
+                    form.enableSubmitButton();
                 });
             }
         });
+        jQuery("#" + MegxFormWidget.cfg.target + "-loading-animation").remove();
     }
 }
 
-MegxFormWidget.prototype.renderForm = function(cfg){
-	var self = this;
-	var alpacaOptions = {
-            "optionsSource": cfg.optionsLocation,
-            "schemaSource": cfg.schemaLocation,
-            "view": {
+MegxFormWidget.prototype.renderForm = function(cfg) {
+    var self = this;
 
-                "parent": "VIEW_JQUERYUI_CREATE",
-                "displayReadonly": true,
+    var alpacaOptions = {
+        "optionsSource": cfg.optionsLocation,
+        "schemaSource": cfg.schemaLocation,
+        "view": {
+            "parent": cfg.parentView,
+            "displayReadonly": true,
+        },
 
-            },
+        "isDynamicCreation": true
+    };
 
-            "isDynamicCreation": true
-        };
+    // now adding wizard to view in case we have one
+    if (typeof cfg.wizard != "undefined" && cfg.wizard) {
+        alpacaOptions.view.wizard = cfg.wizard;
+    }
 
+    if (typeof cfg.postRender != "undefined" && cfg.postRender) {
+        alpacaOptions.postRender = cfg.postRender;
+    } else {
+        alpacaOptions.postRender = self.submitForm;
+    }
 
-        var buts = {
-            "done": {
-                "validateOnClick": true,
-                "onClick": function() {
-                    //        			alert("done clicked."); 
-                    self.submitForm(jQuery("#" + cfg.formId))
-                }
-            },
-
-            "next": {
-                "validateOnClick": true,
-                "onClick": function() {
-                    //            alert("Fired next.onClick handler");
-                }
-            },
-            "prev": {
-                "validateOnClick": true,
-                "onClick": function() {
-                    //alert("Fired prev.onClick handler");
-                }
-            }
-        };
-        // now adding wizard to view in case we have one
-        if (typeof cfg.wizard != "undefined" && cfg.wizard) {
-            alpacaOptions.view.wizard = cfg.wizard;
-            alpacaOptions.view.wizard.buttons = buts;
-        }
-
-        if (typeof cfg.postRender != "undefined" && cfg.postRender) {
-            alpacaOptions.postRender = cfg.postRender;
-        } else {
-            alpacaOptions.postRender = self.submitForm;
-        }
-
-        jQuery("#" + cfg.target).alpaca(alpacaOptions);
+    jQuery("#" + cfg.target).alpaca(alpacaOptions);
 
 }
