16bb10b3195096881d8577935e7219db80fe59a5
bjadroski <bjadroski@82e9701a-15f3-4962-89cc-9b74a964515e>
2015-02-11 15:03:02 +0000

    Improvements of megx-datatable-widget

--- megx.net/trunk/net.megx.datatable-widget/res/net.megx.datatable-widget/js/megx-datatable-widget.js
+++ megx.net/trunk/net.megx.datatable-widget/res/net.megx.datatable-widget/js/megx-datatable-widget.js
@@ -1,57 +1,63 @@
-MegxDatatableWidget = function(config) {
-	var self = this;
-	self.tableId = "t_" + config.tableDivId;
-	self.generateDatatableHtml(config);
-	self.renderDatatable(config);
+var MegxDatatableWidget = function() {
+    var config = {};
+    var generateDatatableHtml = function() {
+        var self = this;
+        var tableDiv = document.getElementById(config.tableDivId);
+        var table = document.createElement("table");
+        table.id = self.tableId;
+        table.setAttribute("class", "display");
+        var thead = document.createElement("thead");
+        var tr = document.createElement("tr");
+        var th;
 
-}
+        for (var i = 0; i < config.columns.length; i++) {
 
-MegxDatatableWidget.prototype.generateDatatableHtml = function(config){
-	var self = this;
-	var tableDiv = document.getElementById(config.tableDivId);
-	var table = document.createElement("table");
-	table.id = self.tableId;
-	table.setAttribute("class", "display");
-	var thead = document.createElement("thead");
-	var tr = document.createElement("tr");
-	
-	for (var i = 0; i < config.columns.length; i++){
-		
-			var th = document.createElement("th");
-			th.innerText = config.columns[i];
-			tr.appendChild(th);
-			
-		}
-	
-	thead.appendChild(tr);
-	var tbody = document.createElement("tbody");
-	table.appendChild(thead);
-	table.appendChild(tbody);
-	tableDiv.appendChild(table);
-	
-}
+            th = document.createElement("th");
+            th.innerText = config.columns[i];
+            tr.appendChild(th);
 
-MegxDatatableWidget.prototype.renderDatatable = function(config){
-	var self = this;
-	var tbl = $("#" + self.tableId);
-	tbl.addClass("megx_dataTable");
-	var cfg = {
-		"bJQueryUI": true,
-		"bPaginate": true,
-		"bFilter": true,
-		"bSort": true,
-		"sPaginationType": "full_numbers",
-		"fnDrawCallback": function( oSettings ) {
-			this.$('tr').hover( function() {
-		        $(this).addClass('highlighted');
-		    }, function() {
-		    	$(this).removeClass('highlighted');
-		    });
-		},
-		"fnOnServerDataLoad": function() {},
-		aoColumns: config.aoColumns
-	};
-	var oTable = tbl.dataTable(cfg);
-	oTable.dataTable().fnAddData(config.data);
+        }
 
-}
+        thead.appendChild(tr);
+        var tbody = document.createElement("tbody");
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        tableDiv.appendChild(table);
+    };
+
+    var renderDatatable = function() {
+        var self = this;
+        var tbl = $("#" + self.tableId);
+        tbl.addClass("megx_dataTable");
+        var cfg = {
+            "bJQueryUI": true,
+            "bPaginate": true,
+            "bFilter": true,
+            "bSort": true,
+            "sPaginationType": "full_numbers",
+            "fnDrawCallback": function(oSettings) {
+                this.$('tr').hover(function() {
+                    $(this).addClass('highlighted');
+                }, function() {
+                    $(this).removeClass('highlighted');
+                });
+            },
+            "fnOnServerDataLoad": function() {},
+            aoColumns: config.aoColumns
+        };
+        var oTable = tbl.dataTable(cfg);
+        oTable.dataTable().fnAddData(config.data);
+    };
+
+    return {
+
+        createTable: function(conf) {
+            config = conf || {};
+            this.tableId = "t_" + config.tableDivId;
+            generateDatatableHtml();
+            renderDatatable();
+        }
+    };
+
+
+};
