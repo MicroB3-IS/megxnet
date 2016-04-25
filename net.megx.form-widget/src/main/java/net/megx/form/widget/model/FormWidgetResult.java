d943e8a59f74b70e48a60728639829dbd1675ec5
Renzo Kottmann <renzok@users.noreply.github.com>
2016-04-25 20:25:46 +0200

    added title attribute to formwidet result

--- net.megx.form-widget/src/main/java/net/megx/form/widget/model/FormWidgetResult.java
+++ net.megx.form-widget/src/main/java/net/megx/form/widget/model/FormWidgetResult.java
@@ -3,6 +3,9 @@ package net.megx.form.widget.model;
 public class FormWidgetResult {
 
   private boolean error;
+  // a short Name of the issue
+  private String title;
+  // a more verbose error statement
   private String message;
   private String redirectUrl;
 
@@ -11,6 +14,14 @@ public class FormWidgetResult {
     this.error = error;
     this.message = message;
     this.redirectUrl = redirectUrl;
+  }
+  
+  public FormWidgetResult(boolean error, String message, String redirectUrl, String title) {
+    super();
+    this.error = error;
+    this.message = message;
+    this.redirectUrl = redirectUrl;
+    this.title = title;
   }
 
   public boolean isError() {
@@ -26,7 +37,9 @@ public class FormWidgetResult {
   }
 
   public void setMessage(String message) {
-    this.message = message;
+    if (message != null ) {
+      this.message = message;
+    }
   }
 
   public String getRedirectUrl() {
@@ -39,8 +52,16 @@ public class FormWidgetResult {
 
   @Override
   public String toString() {
-    return "FormWidgetResult [error=" + error + ", message=" + message
-        + ", redirectUrl=" + redirectUrl + "]";
+    return "FormWidgetResult [title=" + title + "error=" + error + ", message="
+        + message + ", redirectUrl=" + redirectUrl + "]";
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
   }
 
 }
