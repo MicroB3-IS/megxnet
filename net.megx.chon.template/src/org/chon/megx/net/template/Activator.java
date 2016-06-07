fdcdf2a3341d6164a2cc0a2b47c72b241c28f0ff
Renzo Kottmann <renzok@users.noreply.github.com>
2016-06-07 10:29:52 +0200

    Added chon extension capability and made it read config from __config/*.json to have gms url avail as velocity variable

--- net.megx.chon.template/src/org/chon/megx/net/template/Activator.java
+++ net.megx.chon.template/src/org/chon/megx/net/template/Activator.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2011 Max Planck Institute for Marine Microbiology 
- * 
+ * Copyright 2011 Max Planck Institute for Marine Microbiology
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -15,18 +15,80 @@
  */
 package org.chon.megx.net.template;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.chon.cms.core.Extension;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
+import org.chon.cms.model.content.IContentNode;
+import org.chon.web.api.Request;
+import org.chon.web.api.Response;
+import org.chon.web.mpac.Action;
+import org.json.JSONObject;
+import org.osgi.framework.BundleContext;
 
 public class Activator extends ResTplConfiguredActivator {
 
-	@Override
-	protected String getName() {
-		return "net.megx.chon.template";
-	}
+  private static final Log log = LogFactory.getLog(Activator.class);
 
-	@Override
-	protected void registerExtensions(JCRApplication app) {
-		
-	}
+  @Override
+  protected String getName() {
+    return "net.megx.chon.template";
+  }
+
+  @Override
+  protected void onAppAdded(BundleContext context, JCRApplication app) {
+    super.onAppAdded(context, app);
+  }
+
+  @Override
+  protected void registerExtensions(JCRApplication app) {
+    log.debug("registering megx template");
+
+
+    final String OPT_GMSBASEURL_KEY = "gmsBaseUrl";
+
+    app.regExtension("megx", new Extension() {
+
+      String gmsBaseUrl = "http://gms-dev.megx.net/map";
+
+      @Override
+      public Map<String, Action> getAdminActons() {
+        // TODO Auto-generated method stub
+        return null;
+      }
+
+      @Override
+      public Map<String, Action> getAjaxActons() {
+        // TODO Auto-generated method stub
+        return null;
+      }
+
+      @Override
+      public Object getTplObject(Request arg0, Response arg1, IContentNode arg2) {
+
+        JSONObject cfg = getConfig();
+
+        // if cfg is null then log as error but continue with empty
+        // Jsonobject in order to apply default config
+
+        if ( cfg == null ) {
+          log.error("Could not load config json getConfig() returned null");
+          cfg = new JSONObject();
+        }
+
+        log.debug("config is=" + cfg);
+        log.debug("creating velocity template object");
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put(OPT_GMSBASEURL_KEY,
+            cfg.optString(OPT_GMSBASEURL_KEY, "http://gms.megx.net"));
+
+        return map;
+      }
+
+    });
+  }
 }
